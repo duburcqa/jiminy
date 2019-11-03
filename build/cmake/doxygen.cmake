ad5359653d4ecffd99c7a6f6b96a1a1b3ffa5ad1
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2019-11-03 12:21:19 +0100

    Filter doxygen stdout during build. Automatically add \file tag to every source file.

--- build/cmake/doxygen.cmake
+++ build/cmake/doxygen.cmake
@@ -1,19 +1,29 @@
 MACRO (BUILD_DOC)
 	# Build the doc
 	# =============
+
+	# Build the C++ documentation.
 	configure_file (${CMAKE_SOURCE_DIR}/build/doc_cpp/jiminy.doxyfile
     ${CMAKE_BINARY_DIR}/jiminy.doxyfile @ONLY)
 	add_custom_target (doc
-		COMMAND doxygen ${CMAKE_BINARY_DIR}/jiminy.doxyfile
+		COMMAND ${CMAKE_COMMAND} -E env ${BASH} -c "\
+			doxygen -b ${CMAKE_BINARY_DIR}/jiminy.doxyfile &> >(\
+			grep -Ei --line-buffered 'warning\|generating')"
 		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 		COMMENT "Generating documentation with Doxygen..."
-		EXCLUDE_FROM_ALL TRUE)
-	# Python documentation.
+		EXCLUDE_FROM_ALL TRUE
+		VERBATIM)
+
+	# Build the Python documentation.
 	configure_file (${CMAKE_SOURCE_DIR}/build/doc_py/jiminy_python.doxyfile
     ${CMAKE_BINARY_DIR}/jiminy_python.doxyfile @ONLY)
 	add_custom_target (doc_py
-		COMMAND doxygen ${CMAKE_BINARY_DIR}/jiminy_python.doxyfile
+		COMMAND ${CMAKE_COMMAND} -E env ${BASH} -c "\
+			doxygen -b ${CMAKE_BINARY_DIR}/jiminy_python.doxyfile &> >(\
+			grep -Ei --line-buffered 'warning\|generating')"
 		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 		COMMENT "Generating Python documentation with Doxygen..."
-		EXCLUDE_FROM_ALL TRUE)
+		EXCLUDE_FROM_ALL TRUE
+		VERBATIM)
+
 ENDMACRO()
