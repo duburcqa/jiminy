f40fa76723b4da1ca9e5c98ef4f18c4796da3d8a
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2020-03-22 15:42:39 +0100

    Update release tag.

--- gym_jiminy/setup.py
+++ gym_jiminy/setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(name = 'gym_jiminy',
-      version = '1.2.2',
+      version = '1.2.3',
       license = 'MIT',
       description = 'Python-native interface between OpenAI Gym and Jiminy open-source simulator.',
       author = 'Alexis Duburcq',
       author_email = 'alexis.duburcq@wandercraft.eu',
       maintainer = 'Wandercraft',
       url = 'https://github.com/Wandercraft/jiminy',
-      download_url = 'https://github.com/Wandercraft/jiminy/archive/1.2.2.tar.gz',
+      download_url = 'https://github.com/Wandercraft/jiminy/archive/1.2.3.tar.gz',
       packages = find_packages('.'),
       package_data = {'gym_jiminy.envs': ['data/**/*']},
       include_package_data = True, # make sure the data folder is included
       install_requires = [
             'gym',
             'stable_baselines',
-            'jiminy-py~=1.1'
+            'jiminy-py~=1.2'
             ]
 )
