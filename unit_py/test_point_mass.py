4593afc63d63263b95773dd6e9f19530833d529c
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2020-04-06 17:06:19 +0200

    Add Unit Test for the friction model.

--- unit_py/test_point_mass.py
+++ unit_py/test_point_mass.py
@@ -91,5 +91,95 @@ class SimulatePointMass(unittest.TestCase):
         self.assertTrue(np.allclose(-log_data['MassBody.FZ'][-1], mass * gravity, atol=TOLERANCE))
         self.assertTrue(np.allclose(self.k_contact * x_jiminy[-1, 2], mass * gravity, atol=TOLERANCE))
 
+    def test_friction_model(self):
+        '''
+        @brief Validate the friction model.
+
+        @details The transition between dry, dry-viscous, and viscous friction is assessed.
+                 The energy variation and the steady state are also compared to the theoretical model.
+        '''
+
+        # Create the engine
+        engine = jiminy.Engine()
+        engine.initialize(self.robot)
+
+        engine_options = engine.get_options()
+        engine_options['contacts']['stiffness'] = self.k_contact
+        engine_options['contacts']['damping'] = self.nu_contact
+        engine_options['contacts']['frictionDry'] = self.dry_friction
+        engine_options['contacts']['frictionViscous'] = self.visc_friction
+        engine_options['contacts']['frictionStictionVel'] = self.v_stiction
+        engine_options['contacts']['frictionStictionRatio'] = self.r_stiction
+        engine_options['contacts']['transitionEps'] = 1.0 / self.k_contact # To avoid assertion failure because of problem regularization
+        engine_options["stepper"]["dtMax"] = self.dtMax
+        engine_options["stepper"]["logInternalStepperSteps"] = True
+        engine.set_options(engine_options)
+
+        # Extract some information about the engine and the robot
+        mass = self.robot.pinocchio_model.inertias[-1].mass
+        gravity = engine.get_options()['world']['gravity'][2]
+
+        # Register a  impulse of force
+        t0 = 0.05
+        dt = 0.8
+        F = 5.0
+        engine.register_force_impulse("MassBody", t0, dt, np.array([F, 0.0, 0.0, 0.0, 0.0, 0.0]))
+
+        # Run simulation
+        x0 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+                       0.0, 0.0, 0.0, 0.0, 0.0, 0.0, ]) # [TX,TY,TZ],[QX,QY,QZ,QW]
+        tf = 1.5
+
+        engine.simulate(tf, x0)
+
+        log_data, _ = engine.get_log()
+        time = log_data['Global.Time']
+        x_jiminy = np.stack([log_data['HighLevelController.' + s]
+                            for s in self.robot.logfile_position_headers + \
+                                     self.robot.logfile_velocity_headers], axis=-1)
+
+        # Validate the stiction model:
+        # check the transition between dry and viscous friction because of stiction phenomena
+        acceleration = log_data['HighLevelController.currentFreeflyerAccelerationLinX']
+        jerk = np.diff(acceleration) / np.diff(time)
+        snap =  np.diff(jerk) / np.diff(time[1:])
+        snap_rel = np.abs(snap / np.max(snap))
+        snap_disc = time[1:-1][snap_rel > 2.0e-4]
+
+        snap_disc_analytical_dry = log_data['Global.Time'][
+            np.logical_and(x_jiminy[:, 7] > self.v_stiction - self.dtMax,
+                           x_jiminy[:, 7] < self.v_stiction + self.dtMax)]
+        snap_disc_analytical_viscous = log_data['Global.Time'][np.logical_and(
+            x_jiminy[:, 7] > (1.0 + self.r_stiction) * self.v_stiction - self.dtMax,
+            x_jiminy[:, 7] < (1.0 + self.r_stiction) * self.v_stiction + self.dtMax)]
+        snap_disc_analytical = np.sort(np.concatenate(
+            (snap_disc_analytical_dry,
+             snap_disc_analytical_viscous,
+             np.array([t0, t0 + self.dtMax, t0 + dt, t0 + dt + self.dtMax]))))
+
+        self.assertTrue(len(snap_disc) == len(snap_disc_analytical))
+        self.assertTrue(np.allclose(snap_disc, snap_disc_analytical, atol=1e-12))
+
+        # Check that the energy increases only when the force is applied
+        E_robot = log_data['HighLevelController.energy']
+        E_diff_robot = np.concatenate((np.diff(E_robot) / np.diff(time), np.array([0.0], dtype=E_robot.dtype)))
+        E_inc_range = log_data['Global.Time'][np.where(E_diff_robot > 1e-5)[0][[0, -1]]]
+        E_inc_range_analytical = np.array([t0, t0 + dt - self.dtMax])
+
+        self.assertTrue(np.allclose(E_inc_range, E_inc_range_analytical, atol=5e-3))
+
+        # Check that the steady state matches the theory
+        # Note that a specific tolerance is used for the acceleration since the steady state is not perfectly reached
+        TOLERANCE_acc = 1e-5
+
+        v_steady = x_jiminy[log_data['Global.Time'] == t0 + dt, 7]
+        v_steady_analytical = - F / (self.visc_friction * mass * gravity)
+        a_steady = acceleration[np.logical_and(log_data['Global.Time'] > t0 + dt - self.dtMax,
+                                               log_data['Global.Time'] < t0 + dt + self.dtMax)]
+
+        self.assertTrue(len(a_steady) == 1)
+        self.assertTrue(a_steady < TOLERANCE_acc)
+        self.assertTrue(np.allclose(v_steady, v_steady_analytical, atol=TOLERANCE))
+
 if __name__ == '__main__':
     unittest.main()
