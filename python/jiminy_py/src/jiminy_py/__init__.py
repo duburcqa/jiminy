6020500d62ac6391d05891a450d4bbe016ffa67d
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2020-04-11 10:41:03 +0200

    Fix Pypi on CI Win32.

--- python/jiminy_py/src/jiminy_py/__init__.py
+++ python/jiminy_py/src/jiminy_py/__init__.py
@@ -1,11 +1,19 @@
 import os as _os
+
+# [Windows] Define the jiminy package as a site-package
+# to enable global import of Eigenpy and Pinocchio
 if _os.name == 'nt':
 	import site as _site
 	_site.addsitedir(_os.path.dirname(_os.path.realpath(__file__)))
 
+# Import eigenpy and Pinocchio, then patch Pinocchio 
+# to fix support of numpy.ndarray as eigen conversion
 try:
-	import eigenpy as _eigenpy
-	import pinocchio as _pinocchio
+	import eigenpy
+	import pinocchio
 	from . import _pinocchio_init as _patch
 except ImportError:
 	pass
+
+# Import core submodule
+import core
