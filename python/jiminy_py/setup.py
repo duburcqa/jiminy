ee39d4642dd610231a6d6340ab73367752c5c51b
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2020-04-18 17:55:50 +0200

    Hot fix.

--- python/jiminy_py/setup.py
+++ python/jiminy_py/setup.py
@@ -19,14 +19,14 @@ class InstallPlatlib(install):
 
 
 setup(name = 'jiminy_py',
-      version = '1.2.22',
+      version = '1.2.23',
       license = 'MIT',
       description = 'Python-native helper methods and wrapping classes for Jiminy open-source simulator.',
       author = 'Alexis Duburcq',
       author_email = 'alexis.duburcq@wandercraft.eu',
       maintainer = 'Wandercraft',
       url = 'https://github.com/Wandercraft/jiminy',
-      download_url = 'https://github.com/Wandercraft/jiminy/archive/1.2.22.tar.gz',
+      download_url = 'https://github.com/Wandercraft/jiminy/archive/1.2.23.tar.gz',
       packages = find_packages('src'),
       package_dir = {'': 'src'},
       package_data = {'jiminy_py': ['**/*.dll', '**/*.so', '**/*.pyd']},
