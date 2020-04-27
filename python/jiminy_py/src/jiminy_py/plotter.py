d8d5b6d5ceeae394433811ffe0e69ff5afdebabf
Matthieu Vigne <matthieu.vigne@wandercraft.eu>
2020-04-27 12:04:46 +0200

    [jiminy_py] Add option for plotter to compare several logfiles.

--- python/jiminy_py/src/jiminy_py/plotter.py
+++ python/jiminy_py/src/jiminy_py/plotter.py
@@ -3,7 +3,11 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from collections import OrderedDict
+from itertools import cycle
 import fnmatch
+import os
 
 from jiminy_py.log import read_log
 
@@ -15,6 +19,9 @@ def main():
 
     parser = argparse.ArgumentParser(description = description_str, formatter_class = argparse.RawTextHelpFormatter)
     parser.add_argument("input", help = "Input logfile.")
+    parser.add_argument("-c", "--compare", type=str, default=None, help = "Colon-separated list of comparison log files:" +\
+        " the same data as the original log will be plotted in the same subplot, with different line styes."+ \
+        " These logfiles must be of the same length and contain the same header as the original log file")
     main_arguments, plotting_commands = parser.parse_known_args()
 
     # Load log file.
@@ -25,6 +32,14 @@ def main():
         print("Available data:")
         print("\n - ".join(log_data.keys()))
         exit(0)
+
+    # Load comparision logs, if any.
+    compare_data = OrderedDict()
+    if main_arguments.compare is not None:
+        for filename in main_arguments.compare.split(':'):
+            compare_data[filename], _ = read_log(filename)
+    # Define linestyle cycle that will be used for comparison logs.
+    linestyles = ["--","-.",":"]
 
     # Parse plotting arguments.
     plotted_elements = []
@@ -50,7 +65,7 @@ def main():
         n_rows = n_rows + 1
         n_cols = np.ceil(n_plot / (1.0 * n_rows))
 
-    _, axs = plt.subplots(nrows=int(n_rows), ncols=int(n_cols), sharex = True)
+    fig, axs = plt.subplots(nrows=int(n_rows), ncols=int(n_cols), sharex = True)
 
     if n_plot == 1:
         axs = np.array([axs])
@@ -61,13 +76,30 @@ def main():
     # Plot each element.
     for i in range(n_plot):
         for name in plotted_elements[i]:
-            axs[i].plot(t, log_data[name], label = name)
-    # Add legend and grid.
+            line = axs[i].plot(t, log_data[name], label = name)
+            linecycler = cycle(linestyles)
+            for c in compare_data:
+                axs[i].plot(t, compare_data[c][name], next(linecycler), color=line[0].get_color())
+    # Add legend and grid for each plot.
     for ax in axs:
         ax.set_xlabel('time (s)')
         ax.legend()
         ax.grid()
+
+    # If a compare plot is present, add overall legend specifying line types.
     plt.subplots_adjust(bottom=0.05, top=0.98, left=0.06, right=0.98, wspace=0.1, hspace=0.05)
+    if len(compare_data) > 0:
+        linecycler = cycle(linestyles)
+        legend_lines = [Line2D([0], [0], color='k')] +\
+                       [Line2D([0], [0],  color='k', linestyle=next(linecycler)) for _ in compare_data]
+        labels = [os.path.basename(main_arguments.input)] +\
+                 [os.path.basename(c) for c in compare_data]
+
+        legend = fig.legend(legend_lines, labels, loc = 'upper center', ncol = 3)
+        # Increase top margin to fit legend
+        fig.canvas.draw()
+        legend_height = legend.get_window_extent().inverse_transformed(fig.transFigure).height
+        plt.subplots_adjust(top = 0.98 - legend_height)
     plt.show()
 
 if __name__ == "__main__":
