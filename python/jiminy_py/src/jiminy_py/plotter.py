cfe6ffe383d1f70e0f09ef953aaa092b671b953a
Matthieu Vigne <matthieu.vigne@wandercraft.eu>
2020-04-30 11:27:57 +0200

    Implement interactive legend.

--- python/jiminy_py/src/jiminy_py/plotter.py
+++ python/jiminy_py/src/jiminy_py/plotter.py
@@ -21,7 +21,8 @@ def main():
     parser.add_argument("input", help = "Input logfile.")
     parser.add_argument("-c", "--compare", type=str, default=None, help = "Colon-separated list of comparison log files:" +\
         " the same data as the original log will be plotted in the same subplot, with different line styes."+ \
-        " These logfiles must be of the same length and contain the same header as the original log file")
+        " These logfiles must be of the same length and contain the same header as the original log file.\n" +\
+        " Note: you can click on the figure top legend to show / hide data from specific files.")
     main_arguments, plotting_commands = parser.parse_known_args()
 
     # Load log file.
@@ -71,15 +72,25 @@ def main():
         axs = np.array([axs])
     axs = axs.flatten()
 
+    # Store lines in dictionnary fine_name -> plotted lines, to toggle visibility.
+    main_name = os.path.basename(main_arguments.input)
+    plotted_lines = {main_name : []}
+    for c in compare_data:
+        plotted_lines[os.path.basename(c)] = []
+
     plt.gcf().canvas.set_window_title(main_arguments.input)
     t = log_data['Global.Time']
     # Plot each element.
     for i in range(n_plot):
         for name in plotted_elements[i]:
             line = axs[i].plot(t, log_data[name], label = name)
+            plotted_lines[main_name].append(line[0])
+
             linecycler = cycle(linestyles)
             for c in compare_data:
-                axs[i].plot(t, compare_data[c][name], next(linecycler), color=line[0].get_color())
+                l = axs[i].plot(t, compare_data[c][name], next(linecycler), color=line[0].get_color())
+                plotted_lines[os.path.basename(c)].append(l[0])
+
     # Add legend and grid for each plot.
     for ax in axs:
         ax.set_xlabel('time (s)')
@@ -90,16 +101,34 @@ def main():
     plt.subplots_adjust(bottom=0.05, top=0.98, left=0.06, right=0.98, wspace=0.1, hspace=0.05)
     if len(compare_data) > 0:
         linecycler = cycle(linestyles)
-        legend_lines = [Line2D([0], [0], color='k')] +\
-                       [Line2D([0], [0],  color='k', linestyle=next(linecycler)) for _ in compare_data]
-        labels = [os.path.basename(main_arguments.input)] +\
-                 [os.path.basename(c) for c in compare_data]
 
-        legend = fig.legend(legend_lines, labels, loc = 'upper center', ncol = 3)
+        # Dictionnary: line in legend to log name.
+        legend_lines = {Line2D([0], [0], color='k') : main_name}
+        for c in compare_data:
+            legend_lines[Line2D([0], [0],  color='k', linestyle=next(linecycler))] = os.path.basename(c)
+
+        legend = fig.legend(legend_lines.keys(), legend_lines.values(), loc = 'upper center', ncol = 3)
+        # Create a map (picker) -> (log name) for both the lines and the legend text.
+        picker_to_name = {}
+        for legline, name in zip(legend.get_lines(), legend_lines.values()):
+            legline.set_picker(10)  # 10 pts tolerance
+            picker_to_name[legline] = name
+        for legline, name in zip(legend.get_texts(), legend_lines.values()):
+            legline.set_picker(10)  # 10 pts tolerance
+            picker_to_name[legline] = name
+
         # Increase top margin to fit legend
         fig.canvas.draw()
         legend_height = legend.get_window_extent().inverse_transformed(fig.transFigure).height
         plt.subplots_adjust(top = 0.98 - legend_height)
+
+        def legend_clicked(event):
+            file_name = picker_to_name[event.artist]
+            for line in plotted_lines[file_name]:
+                line.set_visible(not line.get_visible())
+            fig.canvas.draw()
+        # Make legend interactive
+        fig.canvas.mpl_connect('pick_event', legend_clicked)
     plt.show()
 
 if __name__ == "__main__":
