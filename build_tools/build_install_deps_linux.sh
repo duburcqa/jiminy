cc50b1278fcbb97083cb17c418b8b7793a7fe62f
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2020-08-10 09:22:35 +0200

    [Linux] Update Jiminy dependencies versions (#161)
    * Do not enforce system-wide Python binary to use by default to avoid breaking user environment.
    * Add missing cmake lookup directory. 
    * Use 'with-' statement to conf boost and specify Python version.
    
    Co-authored-by: Alexis Duburcq <alexis.duburcq@wandercraft.eu>

--- build_tools/build_install_deps_linux.sh
+++ build_tools/build_install_deps_linux.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+set -eo
+
 ################################## Configure the environment ###########################################
 
 ### Set the build type to "Release" if undefined
-if [ -z ${var+x} ]; then
+if [ -z ${BUILD_TYPE} ]; then
   BUILD_TYPE="Release"
+  echo "BUILD_TYPE is unset. Defaulting to 'Release'."
 fi
 
 ### Get the fullpath of Jiminy project
@@ -13,22 +17,23 @@ RootDir="$(dirname $ScriptDir)"
 InstallDir="$RootDir/install"
 mkdir -p "$InstallDir"
 
-### Eigenpy and Pinocchio are using the deprecated FindPythonInterp cmake helper to detect Python executable,
-#   which is not working properly when several executables exist.
-PYTHON_EXECUTABLE=$(python -c "import sys; sys.stdout.write(sys.executable)")
+### Eigenpy and Pinocchio are using the deprecated FindPythonInterp
+#   cmake helper to detect Python executable, which is not working
+#   properly when several executables exist.
+if [ -z ${PYTHON_EXECUTABLE} ]; then
+  PYTHON_EXECUTABLE=$(python -c "import sys; sys.stdout.write(sys.executable)")
+  echo "PYTHON_EXECUTABLE is unset. Defaulting to '${PYTHON_EXECUTABLE}'."
+fi
 
 ### Remove the preinstalled boost library from search path
 unset Boost_ROOT
-
-### Add the generated pkgconfig file to the search path
-export PKG_CONFIG_PATH="$InstallDir/lib/pkgconfig;$InstallDir/lib64/pkgconfig;$InstallDir/share/pkgconfig"
 
 ################################## Checkout the dependencies ###########################################
 
 ### Checkout boost and its submodules.
 #   Boost numeric odeint < 1.71 does not support eigen3 > 3.2,
 #   and eigen < 3.3 build fails on windows because of a cmake error
-git clone -b "boost-1.71.0" https://github.com/boostorg/boost.git "$RootDir/boost"
+git clone -b "boost-1.72.0" https://github.com/boostorg/boost.git "$RootDir/boost"
 cd "$RootDir/boost"
 git submodule --quiet update --init --recursive --jobs 8
 
@@ -36,7 +41,7 @@ git submodule --quiet update --init --recursive --jobs 8
 git clone -b "3.3.7" https://github.com/eigenteam/eigen-git-mirror.git "$RootDir/eigen3"
 
 ### Checkout eigenpy and its submodules
-git clone -b "v2.3.1" https://github.com/stack-of-tasks/eigenpy.git "$RootDir/eigenpy"
+git clone -b "v2.4.3" https://github.com/stack-of-tasks/eigenpy.git "$RootDir/eigenpy"
 cd "$RootDir/eigenpy"
 git submodule --quiet update --init --recursive --jobs 8
 
@@ -47,13 +52,13 @@ git clone -b "master" https://github.com/robotology-dependencies/tinyxml.git "$R
 git clone -b "0.4.4" https://github.com/ros/console_bridge.git "$RootDir/console_bridge"
 
 ### Checkout urdfdom_headers
-git clone -b "1.0.3" https://github.com/ros/urdfdom_headers.git "$RootDir/urdfdom_headers"
+git clone -b "1.0.5" https://github.com/ros/urdfdom_headers.git "$RootDir/urdfdom_headers"
 
 ### Checkout urdfdom
-git clone -b "1.0.3" https://github.com/ros/urdfdom.git "$RootDir/urdfdom"
+git clone -b "1.0.4" https://github.com/ros/urdfdom.git "$RootDir/urdfdom"
 
 ### Checkout pinocchio and its submodules
-git clone -b "v2.4.4" https://github.com/stack-of-tasks/pinocchio.git "$RootDir/pinocchio"
+git clone -b "v2.4.7" https://github.com/stack-of-tasks/pinocchio.git "$RootDir/pinocchio"
 cd "$RootDir/pinocchio"
 git submodule --quiet update --init --recursive --jobs 8
 
@@ -68,19 +73,26 @@ git submodule --quiet update --init --recursive --jobs 8
 
 ### Build and install the build tool b2 (build-ception !)
 cd "$RootDir/boost"
-./bootstrap.sh --prefix="$InstallDir"
+./bootstrap.sh --prefix="$InstallDir" --with-python="${PYTHON_EXECUTABLE}"
+
+### File "project-config.jam" create by bootstrap must be edited manually
+#   to specify Python included dir manually, since it is not detected
+#   sucessfully in some cases.
+PYTHON_VERSION="$(${PYTHON_EXECUTABLE} -c "import sys; print (\"%d.%d\" % (sys.version_info[0], sys.version_info[1]))")"
+PYTHON_ROOT="$(${PYTHON_EXECUTABLE} -c "import sys; print(sys.prefix)")"
+PYTHON_INCLUDE_DIRS="$(${PYTHON_EXECUTABLE} -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_python_inc())")"
+PYTHON_CONFIG_JAM="using python : ${PYTHON_VERSION} : ${PYTHON_ROOT} : ${PYTHON_INCLUDE_DIRS} ;"
+sed -i "/using python/c ${PYTHON_CONFIG_JAM}" ./project-config.jam
 
 ### Build and install and install boost (Replace -d0 option by -d1 to check compilation errors)
 BuildTypeB2="$(echo "$BUILD_TYPE" | tr '[:upper:]' '[:lower:]')"
 mkdir -p "$RootDir/boost/build"
 ./b2 --prefix="$InstallDir" --build-dir="$RootDir/boost/build" \
-     --without-wave --without-contract --without-graph --without-regex \
-     --without-mpi --without-coroutine --without-fiber --without-context \
-     --without-timer --without-chrono --without-atomic --without-graph_parallel \
-     --without-type_erasure --without-container --without-exception --without-locale \
-     --without-log --without-program_options --without-random --without-iostreams \
-     --build-type=minimal toolset=gcc variant="$BuildTypeB2" threading=multi --layout=system \
-     architecture=x86 address-model=64 link=shared runtime-link=shared install -q -d0 -j2
+     --with-date_time --with-filesystem --with-headers --with-math --with-python \
+     --with-serialization --with-stacktrace --with-system --with-test --with-thread \
+     --with-python --build-type=minimal architecture=x86 address-model=64 \
+     threading=multi --layout=system link=shared runtime-link=shared \
+     toolset=gcc cxxflags="-std=c++11 -fPIC" variant="$BuildTypeB2" install -q -d0 -j2
 
 #################################### Build and install eigen3 ##########################################
 
@@ -115,17 +127,12 @@ sed -i 's/SHARED //g' "$RootDir/eigenpy/CMakeLists.txt"
 mkdir -p "$RootDir/eigenpy/build"
 cd "$RootDir/eigenpy/build"
 cmake "$RootDir/eigenpy" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
-      -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE \
+      -DCMAKE_PREFIX_PATH="$InstallDir" -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
+      -DPYTHON_STANDARD_LAYOUT=ON -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE \
       -DBOOST_ROOT="$InstallDir" -DBoost_INCLUDE_DIR="$InstallDir/include" \
-      -DBoost_USE_STATIC_LIBS=OFF -DBUILD_TESTING=OFF \
+      -DBUILD_TESTING=OFF -DINSTALL_DOCUMENTATION=OFF -DBoost_USE_STATIC_LIBS=OFF \
       -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
-
-### Embedded the required dynamic library in the package folder
-# cp "$InstallDir/bin/eigenpy.dll" \
-#           -Destination "$InstallDir/lib/site-packages/eigenpy"
-# cp "$InstallDir/lib/boost_python*.dll" \
-#           -Destination "$InstallDir/lib/site-packages/eigenpy"
 
 ################################## Build and install tinyxml ###########################################
 
@@ -173,7 +180,7 @@ sed -i 's/SHARED //g' "$RootDir/urdfdom/urdf_parser/CMakeLists.txt"
 mkdir -p "$RootDir/urdfdom/build"
 cd "$RootDir/urdfdom/build"
 cmake "$RootDir/urdfdom" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
-      -DBUILD_TESTING=OFF \
+      -DCMAKE_PREFIX_PATH="$InstallDir" -DBUILD_TESTING=OFF \
       -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC -DURDFDOM_STATIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
 
@@ -196,18 +203,6 @@ sed -i '73s/.*/ /' "$RootDir/pinocchio/cmake/boost.cmake"
 ### Must patch /src/CMakefile.txt to disable library type enforced SHARED
 sed -i 's/SHARED //g' "$RootDir/pinocchio/src/CMakeLists.txt"
 
-### Must patch line 117 of bindings/python/CMakeLists.txt to use the right Python library name
-sed -i '117s/.*/'"\
-"'SET_TARGET_PROPERTIES(${PYWRAP} PROPERTIES '"\
-"'PREFIX "" '"\
-"'SUFFIX "${PYTHON_EXT_SUFFIX}" '"\
-"'OUTPUT_NAME "${PYWRAP}") /' \
-"$RootDir/pinocchio/bindings/python/CMakeLists.txt"
-
-### Replace recursively in all files the Python library name by the right one
-find "$RootDir/pinocchio" -type f \( -name "*.py" -o -name "*.cpp" \) \
--exec sed -i'' -e 's/libpinocchio_pywrap/pinocchio_pywrap/g' {} +
-
 ### Remove every std::vector bindings of native types, since it makes absolutely no sense to bind such ambiguous types
 find "$RootDir/pinocchio" -type f -name "*.hpp" -exec ex -sc "g/StdVectorPythonVisitor</d" -cx {} ';'
 
@@ -215,9 +210,10 @@ find "$RootDir/pinocchio" -type f -name "*.hpp" -exec ex -sc "g/StdVectorPythonV
 mkdir -p "$RootDir/pinocchio/build"
 cd "$RootDir/pinocchio/build"
 cmake "$RootDir/pinocchio" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
-      -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE \
+      -DCMAKE_PREFIX_PATH="$InstallDir" -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
+      -DPYTHON_STANDARD_LAYOUT=ON -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE \
       -DBOOST_ROOT="$InstallDir" -DBoost_INCLUDE_DIR="$InstallDir/include" \
-      -DBoost_USE_STATIC_LIBS=OFF -DBUILD_WITH_COLLISION_SUPPORT=OFF -DBUILD_TESTING=OFF \
-      -DBUILD_WITH_URDF_SUPPORT=ON -DBUILD_PYTHON_INTERFACE=ON \
+      -DBUILD_WITH_COLLISION_SUPPORT=OFF -DBUILD_TESTING=OFF -DINSTALL_DOCUMENTATION=OFF \
+      -DBUILD_WITH_URDF_SUPPORT=ON -DBUILD_PYTHON_INTERFACE=ON -DBoost_USE_STATIC_LIBS=OFF \
       -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC -DURDFDOM_STATIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
