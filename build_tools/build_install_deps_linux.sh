a2074ac82546546fa3307eb69dcf4bd78daf2945
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2020-08-11 21:38:46 +0200

    [Win10] Update Jiminy dependencies versions (#162)
    * Dependency version parity between Linux and Windows.
    * Use unified diff files to patch dependencies
    * Provide 'recording' tools for viewer as Python extra requirement.
    * Enable C++14 for compiling dependencies.
    * Get rid of Pkgconfig.
    
    Co-authored-by: Alexis Duburcq <alexis.duburcq@wandercraft.eu>

--- build_tools/build_install_deps_linux.sh
+++ build_tools/build_install_deps_linux.sh
@@ -33,7 +33,8 @@ unset Boost_ROOT
 ### Checkout boost and its submodules.
 #   Boost numeric odeint < 1.71 does not support eigen3 > 3.2,
 #   and eigen < 3.3 build fails on windows because of a cmake error
-git clone -b "boost-1.72.0" https://github.com/boostorg/boost.git "$RootDir/boost"
+#   Note that Boost 1.72 is not yet officially supported by Cmake 3.16, which is the "default" version used on Windows 10.
+git clone -b "boost-1.71.0" https://github.com/boostorg/boost.git "$RootDir/boost"
 cd "$RootDir/boost"
 git submodule --quiet update --init --recursive --jobs 8
 
@@ -44,9 +45,12 @@ git clone -b "3.3.7" https://github.com/eigenteam/eigen-git-mirror.git "$RootDir
 git clone -b "v2.4.3" https://github.com/stack-of-tasks/eigenpy.git "$RootDir/eigenpy"
 cd "$RootDir/eigenpy"
 git submodule --quiet update --init --recursive --jobs 8
+git apply --reject --whitespace=fix "$RootDir/build_tools/patch_deps_linux/eigenpy.patch"
 
 ### Checkout tinyxml (robotology fork for cmake compatibility)
 git clone -b "master" https://github.com/robotology-dependencies/tinyxml.git "$RootDir/tinyxml"
+cd "$RootDir/tinyxml"
+git apply --reject --whitespace=fix "$RootDir/build_tools/patch_deps_linux/tinyxml.patch"
 
 ### Checkout console_bridge
 git clone -b "0.4.4" https://github.com/ros/console_bridge.git "$RootDir/console_bridge"
@@ -56,11 +60,14 @@ git clone -b "1.0.5" https://github.com/ros/urdfdom_headers.git "$RootDir/urdfdo
 
 ### Checkout urdfdom
 git clone -b "1.0.4" https://github.com/ros/urdfdom.git "$RootDir/urdfdom"
+cd "$RootDir/urdfdom"
+git apply --reject --whitespace=fix "$RootDir/build_tools/patch_deps_linux/urdfdom.patch"
 
 ### Checkout pinocchio and its submodules
 git clone -b "v2.4.7" https://github.com/stack-of-tasks/pinocchio.git "$RootDir/pinocchio"
 cd "$RootDir/pinocchio"
 git submodule --quiet update --init --recursive --jobs 8
+git apply --reject --whitespace=fix "$RootDir/build_tools/patch_deps_linux/pinocchio.patch"
 
 ################################### Build and install boost ############################################
 
@@ -84,7 +91,8 @@ PYTHON_INCLUDE_DIRS="$(${PYTHON_EXECUTABLE} -c "import distutils.sysconfig as sy
 PYTHON_CONFIG_JAM="using python : ${PYTHON_VERSION} : ${PYTHON_ROOT} : ${PYTHON_INCLUDE_DIRS} ;"
 sed -i "/using python/c ${PYTHON_CONFIG_JAM}" ./project-config.jam
 
-### Build and install and install boost (Replace -d0 option by -d1 to check compilation errors)
+### Build and install and install boost
+#   (Replace -d0 option by -d1 and remove -q option to check compilation errors)
 BuildTypeB2="$(echo "$BUILD_TYPE" | tr '[:upper:]' '[:lower:]')"
 mkdir -p "$RootDir/boost/build"
 ./b2 --prefix="$InstallDir" --build-dir="$RootDir/boost/build" \
@@ -98,35 +106,16 @@ mkdir -p "$RootDir/boost/build"
 
 mkdir -p "$RootDir/eigen3/build"
 cd "$RootDir/eigen3/build"
-cmake "$RootDir/eigen3" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/eigen3" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DBUILD_TESTING=OFF -DEIGEN_BUILD_PKGCONFIG=ON \
       -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
 
 ################################### Build and install eigenpy ##########################################
 
-### Must patch line 92 of cmake/python.cmake for centOS 6 and above to avoid looking for PYTHON_LIBRARY,
-#   since it is both irrelevant and failing in some cases. Indeed, it is the case for
-#   the official 'manylinux2010' docker image, for which such library is not available.
-if ( rpm -q centos-release >/dev/null 2>&1 ) ; then
-    sed -i '92s/.*/'"\
-    "'FIND_PACKAGE("Python${_PYTHON_VERSION_MAJOR}" COMPONENTS Interpreter) \n '"\
-    "'execute_process(COMMAND "${Python${_PYTHON_VERSION_MAJOR}_EXECUTABLE}" -c '"\
-    "'                        "import distutils.sysconfig as sysconfig; print(sysconfig.get_python_inc())" '"\
-    "'                OUTPUT_STRIP_TRAILING_WHITESPACE '"\
-    "'                OUTPUT_VARIABLE Python${_PYTHON_VERSION_MAJOR}_INCLUDE_DIRS) /' \
-    "$RootDir/eigenpy/cmake/python.cmake"
-fi
-
-### Remove line 73 of boost.cmake to disable library type enforced SHARED
-sed -i '73s/.*/ /' "$RootDir/eigenpy/cmake/boost.cmake"
-
-### Must patch /CMakefile.txt to disable library type enforced SHARED
-sed -i 's/SHARED //g' "$RootDir/eigenpy/CMakeLists.txt"
-
 mkdir -p "$RootDir/eigenpy/build"
 cd "$RootDir/eigenpy/build"
-cmake "$RootDir/eigenpy" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/eigenpy" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DCMAKE_PREFIX_PATH="$InstallDir" -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
       -DPYTHON_STANDARD_LAYOUT=ON -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE \
       -DBOOST_ROOT="$InstallDir" -DBoost_INCLUDE_DIR="$InstallDir/include" \
@@ -136,15 +125,9 @@ make install -j2
 
 ################################## Build and install tinyxml ###########################################
 
-### Patch line 19 of CMakeLists.txt to set the right default library directory depending to the linux distro.
-sed -i '19s/.*/'\
-'include(GNUInstallDirs) \n '\
-'set(TINYXML_INSTALL_LIB_DIR "${CMAKE_INSTALL_LIBDIR}" CACHE PATH "Installation directory for libraries") /' \
-"$RootDir/tinyxml/CMakeLists.txt"
-
 mkdir -p "$RootDir/tinyxml/build"
 cd "$RootDir/tinyxml/build"
-cmake "$RootDir/tinyxml" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/tinyxml" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
 
@@ -152,7 +135,7 @@ make install -j2
 
 mkdir -p "$RootDir/console_bridge/build"
 cd "$RootDir/console_bridge/build"
-cmake "$RootDir/console_bridge" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/console_bridge" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
 
@@ -160,60 +143,29 @@ make install -j2
 
 mkdir -p "$RootDir/urdfdom_headers/build"
 cd "$RootDir/urdfdom_headers/build"
-cmake "$RootDir/urdfdom_headers" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/urdfdom_headers" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
 
 ################################# Build and install urdfdom ###########################################
 
-### Must patch line 71 of CMakeLists.txt to add TinyXML dependency to cmake configuration files generator
-sed -i '71s/.*/ set(PKG_DEPENDS urdfdom_headers console_bridge TinyXML) /' \
-"$RootDir/urdfdom/CMakeLists.txt"
-
-### Must patch line 81 of CMakeLists.txt to add TinyXML dependency to pkgconfig files generator
-sed -i '81s/.*/ set(PKG_URDF_LIBS "-lurdfdom_sensor -lurdfdom_model_state -lurdfdom_model -lurdfdom_world -ltinyxml") /' \
-"$RootDir/urdfdom/CMakeLists.txt"
-
-### Must patch /urdf_parser/CMakeLists.txt to disable library type enforced SHARED
-sed -i 's/SHARED //g' "$RootDir/urdfdom/urdf_parser/CMakeLists.txt"
-
 mkdir -p "$RootDir/urdfdom/build"
 cd "$RootDir/urdfdom/build"
-cmake "$RootDir/urdfdom" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/urdfdom" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DCMAKE_PREFIX_PATH="$InstallDir" -DBUILD_TESTING=OFF \
-      -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC -DURDFDOM_STATIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
+      -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
 
 ################################ Build and install Pinocchio ##########################################
 
-### Must patch line 92 of cmake/python.cmake for centOS 6 and above.
-if ( rpm -q centos-release >/dev/null 2>&1 ) ; then
-    sed -i '92s/.*/'"\
-    "'FIND_PACKAGE("Python${_PYTHON_VERSION_MAJOR}" COMPONENTS Interpreter) \n '"\
-    "'execute_process(COMMAND "${Python${_PYTHON_VERSION_MAJOR}_EXECUTABLE}" -c '"\
-    "'                        "import distutils.sysconfig as sysconfig; print(sysconfig.get_python_inc())" '"\
-    "'                OUTPUT_STRIP_TRAILING_WHITESPACE '"\
-    "'                OUTPUT_VARIABLE Python${_PYTHON_VERSION_MAJOR}_INCLUDE_DIRS) /' \
-    "$RootDir/pinocchio/cmake/python.cmake"
-fi
-
-### Remove line 73 of boost.cmake to disable library type enforced SHARED
-sed -i '73s/.*/ /' "$RootDir/pinocchio/cmake/boost.cmake"
-
-### Must patch /src/CMakefile.txt to disable library type enforced SHARED
-sed -i 's/SHARED //g' "$RootDir/pinocchio/src/CMakeLists.txt"
-
-### Remove every std::vector bindings of native types, since it makes absolutely no sense to bind such ambiguous types
-find "$RootDir/pinocchio" -type f -name "*.hpp" -exec ex -sc "g/StdVectorPythonVisitor</d" -cx {} ';'
-
 ### Build and install pinocchio, finally !
 mkdir -p "$RootDir/pinocchio/build"
 cd "$RootDir/pinocchio/build"
-cmake "$RootDir/pinocchio" -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
+cmake "$RootDir/pinocchio" -DCMAKE_CXX_STANDARD=14 -DCMAKE_INSTALL_PREFIX="$InstallDir" \
       -DCMAKE_PREFIX_PATH="$InstallDir" -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
       -DPYTHON_STANDARD_LAYOUT=ON -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_NO_BOOST_CMAKE=TRUE \
       -DBOOST_ROOT="$InstallDir" -DBoost_INCLUDE_DIR="$InstallDir/include" \
       -DBUILD_WITH_COLLISION_SUPPORT=OFF -DBUILD_TESTING=OFF -DINSTALL_DOCUMENTATION=OFF \
       -DBUILD_WITH_URDF_SUPPORT=ON -DBUILD_PYTHON_INTERFACE=ON -DBoost_USE_STATIC_LIBS=OFF \
-      -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC -DURDFDOM_STATIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
+      -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make install -j2
