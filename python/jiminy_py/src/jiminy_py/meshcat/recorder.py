a1b5365272e3c90c630578580a46482d5f5bb027
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2020-08-26 14:10:48 +0200

    [Viewer] Speedup meshcat video recording (#182)
    * Viewer Meshcat meshes_loaded now support specified the scene and robot name.
    * Add dedicate meshcat wrapper which handles video recording directly. 
    * Update WebM Writer directly to enable framerate and quality setting.
    * Dramatically improve recording speed using meshcat from 2FPS to 10FPS.
    
    Co-authored-by: Alexis Duburcq <alexis.duburcq@wandercraft.eu>

--- python/jiminy_py/src/jiminy_py/meshcat/recorder.py
+++ python/jiminy_py/src/jiminy_py/meshcat/recorder.py
@@ -1,12 +1,18 @@
 import os
 import sys
+import time
 import signal
 import atexit
+import psutil
+import pathlib
 import asyncio
 import subprocess
 import multiprocessing
 from ctypes import c_char_p, c_bool, c_int
 from contextlib import redirect_stderr
+
+# Must use a recent release that supports webgl rendering with hardware acceleration
+os.environ['PYPPETEER_CHROMIUM_REVISION'] = '801225'
 
 from pyppeteer.connection import Connection
 from pyppeteer.browser import Browser
@@ -26,15 +32,16 @@ async def launch(self) -> Browser:
     options = dict()
     options['env'] = self.env
     cmd = self.cmd + [
+        "--enable-webgl",
+        "--use-gl=egl",
+        "--disable-frame-rate-limit",
+        "--disable-gpu-vsync",
         "--ignore-certificate-errors",
         "--disable-infobars",
         "--disable-breakpad",
         "--disable-setuid-sandbox",
         "--proxy-server='direct://'",
-        "--proxy-bypass-list=*",
-        "--enable-webgl",
-        "--disable-frame-rate-limit",
-        "--disable-gpu-vsync"]
+        "--proxy-bypass-list=*"]
     if not self.dumpio:
         options['stdout'] = subprocess.PIPE
         options['stderr'] = subprocess.STDOUT
@@ -46,17 +53,6 @@ async def launch(self) -> Browser:
     else:
         self.proc = subprocess.Popen(
             cmd, **options, preexec_fn=os.setpgrp, shell=False)
-
-    # don't forget to close browser process
-    def _close_process(*args, **kwargs) -> None:
-        if not self.chromeClosed:
-            self._loop.run_until_complete(self.killChrome())
-    atexit.register(_close_process)
-    if self.handleSIGTERM:
-        signal.signal(signal.SIGTERM, _close_process)
-    if not sys.platform.startswith('win'):
-        if self.handleSIGHUP:
-            signal.signal(signal.SIGHUP, _close_process)
 
     self.browserWSEndpoint = get_ws_endpoint(self.url)
     self.connection = Connection(self.browserWSEndpoint, self._loop)
@@ -73,36 +69,67 @@ Launcher.launch = launch
 
 # ======================================================
 
-def capture_frame(client, width, height):
+async def capture_frame_async(client, width, height):
     """
     @brief    Send a javascript command to the hidden browser to
               capture frame, then wait for it (since it is async).
     """
-    async def capture_frame_async(client):
-        nonlocal width, height
-        _width = client.html.page.viewport['width']
-        _height = client.html.page.viewport['height']
-        if not width > 0:
-            width = _width
-        if not height > 0:
-            height = _height
-        if _width != width or _height != height:
-            await client.html.page.setViewport(
-                {'width': width, 'height': height})
-        return await client.html.page.evaluate("""
-            () => {
-                return viewer.capture_image();
-            }
-        """)
-    loop = asyncio.get_event_loop()
-    img_data = loop.run_until_complete(capture_frame_async(client))
-    return img_data
+    _width = client.html.page.viewport['width']
+    _height = client.html.page.viewport['height']
+    if not width > 0:
+        width = _width
+    if not height > 0:
+        height = _height
+    if _width != width or _height != height:
+        await client.html.page.setViewport(
+            {'width': width, 'height': height})
+    return await client.html.page.evaluate("""
+        () => {
+            return viewer.capture_image();
+        }
+    """)
 
-def meshcat_recorder(meshcat_url,
-                     take_snapshot_shm,
-                     img_data_html_shm,
-                     width_shm,
-                     height_shm):
+async def start_video_recording_async(client, fps, width, height):
+    await client.html.page.setViewport(
+            {'width': width, 'height': height})
+    await client.html.page.evaluate(f"""
+        () => {{
+            stop_animate();
+            viewer.animator.capturer = new WebMWriter({{
+                quality: 0.99999,  // Lossless codex VP8L is not supported
+                frameRate: {fps}
+
+            }});
+        }}
+    """)
+
+async def add_video_frame_async(client):
+    await client.html.page.evaluate("""
+        () => {
+            viewer.renderer.render(viewer.scene, viewer.camera);
+            viewer.animator.capturer.addFrame(viewer.renderer.domElement);
+        }
+    """)
+
+async def stop_and_save_video_async(client, path):
+    directory = os.path.dirname(path)
+    filename = os.path.splitext(os.path.basename(path))[0]
+    await client.html.page._client.send('Page.setDownloadBehavior',
+        {'behavior': 'allow', 'downloadPath': directory})
+    await client.html.page.evaluate(f"""
+        () => {{
+            viewer.animator.capturer.complete().then(function(blob) {{
+                const a = document.createElement('a');
+                const url = URL.createObjectURL(blob);
+                a.href = url;
+                a.download = "{filename}";
+                a.click();
+            }});
+            start_animate();
+        }}
+    """)
+
+def meshcat_recorder(meshcat_url, request_shm, message_shm):
     # Do not catch signal interrupt automatically, to avoid killing meshcat
     # server and stopping Jupyter notebook cell.
     signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -112,52 +139,153 @@ def meshcat_recorder(meshcat_url,
     client = session.get(meshcat_url)
     client.html.render(keep_page=True)
 
-    # Stop rendering loop since it is irrelevant in his case, because
-    # capture_frame is already doing the job.
-    async def stop_animation_async(client):
-        return await client.html.page.evaluate("""
-            () => {
-                stop_animate();
-            }
-        """)
+    # Infinite loop, waiting for requests
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(stop_animation_async(client))
-
     with open(os.devnull, 'w') as f:
         with redirect_stderr(f):
-            while True:
-                # Wait for request to take a screenshot
-                if take_snapshot_shm.value:
-                    img_data_html_shm.value = capture_frame(
-                        client, width_shm.value, height_shm.value)
-                    take_snapshot_shm.value = False
+            while request_shm.value != "quit": # (request := request_shm.value) != "quit":
+                request = request_shm.value
+                if request != "":
+                    args = list(map(str.strip, message_shm.value.split(",")))
+                    if request == "take_snapshot":
+                        width, height = map(int, args)
+                        coro = capture_frame_async(client, width, height)
+                    elif request == "start_record":
+                        fps, width, height = map(int, args)
+                        coro = start_video_recording_async(
+                            client, fps, width, height)
+                    elif request == "add_frame":
+                        coro = add_video_frame_async(client)
+                    elif request == "stop_and_save_record":
+                        (path,) = args
+                        coro = stop_and_save_video_async(client, path)
+                    else:
+                        continue
+                    try:
+                        output = loop.run_until_complete(coro)
+                        if output is not None:
+                            message_shm.value = output
+                        request_shm.value = ""
+                    except Exception as e:
+                        print(e)
+                        message_shm.value = str(e)
+                        request_shm.value = "quit"
+            request_shm.value = ""
+    session.close()
 
-def mgr_init():
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-def start_meshcat_recorder(meshcat_url):
+class MeshcatRecorder:
     """
     @brief    Run meshcat server in background using multiprocessing
               Process to enable parallel asyncio loop execution, which
               is necessary to support recording in Jupyter notebook.
     """
-    manager = multiprocessing.managers.SyncManager()
-    manager.start(mgr_init)
-    recorder_shm = {
-        'take_snapshot': manager.Value(c_bool, False),
-        'img_data_html': manager.Value(c_char_p, ""),
-        'width': manager.Value(c_int, -1),
-        'height': manager.Value(c_int, -1)
-    }
+    def __init__(self, url):
+        self.is_open = False
+        self.is_recording = False
+        self.url = url
+        self.__manager = None
+        self.__shm = None
+        self.proc = None
 
-    recorder = multiprocessing.Process(
-        target=meshcat_recorder,
-        args=(meshcat_url,
-            recorder_shm['take_snapshot'],
-            recorder_shm['img_data_html'],
-            recorder_shm['width'],
-            recorder_shm['height']),
-        daemon=True)
-    recorder.start()
+    def open(self):
+        self.__manager = multiprocessing.managers.SyncManager()
+        self.__manager.start(
+            lambda : signal.signal(signal.SIGINT, signal.SIG_IGN))
 
-    return recorder, manager, recorder_shm
+        self.__shm = {
+            'request': self.__manager.Value(c_char_p, ""),
+            'message': self.__manager.Value(c_char_p, "")
+        }
+
+        self.proc = multiprocessing.Process(
+            target=meshcat_recorder,
+            args=(self.url, self.__shm['request'], self.__shm['message']),
+            daemon=True)
+        self.proc.start()
+
+        self.is_open = True
+
+    def __del__(self):
+        self.release()
+
+    def release(self):
+        if self.__shm is not None:
+            if self.proc.is_alive():
+                self._send_request(request="quit")
+            self.__shm = None
+        if self.proc is not None:
+            self.proc.terminate()
+            self.proc = None
+        if self.__manager is not None:
+            self.__manager.shutdown()
+            self.__manager = None
+        self.is_open = False
+
+    def _send_request(self, request, message=None, timeout=10.0):
+        if not self.is_open:
+            raise RuntimeError(
+                "Meshcat recorder is not open. Impossible to send requests.")
+        if message is not None:
+            self.__shm['message'].value = message
+        else:
+            self.__shm['message'].value = ""
+        self.__shm['request'].value = request
+        timeout += time.time()
+        try:
+            while self.__shm['request'].value != "":
+                if time.time() > timeout:
+                    self.release()
+                    raise RuntimeError("Timeout.")
+                elif not self.proc.is_alive():
+                    self.release()
+                    raise RuntimeError(
+                        "Backend browser has encountered an unrecoverable "\
+                        "error: ", self.__shm['message'].value)
+        except KeyboardInterrupt:
+            self.__shm['request'].value = ""
+
+    def capture_frame(self, width=None, height=None):
+        self._send_request("take_snapshot",
+            message=f"{width if width is not None else -1},"\
+                    f"{height if height is not None else -1}")
+        return self.__shm['message'].value
+
+    def start_video_recording(self, fps, width, height):
+        self._send_request("start_record",
+            message=f"{fps},{width},{height}")
+        self.is_recording = True
+
+    def add_video_frame(self):
+        if not self.is_recording:
+            raise RuntimeError(
+                "No video being recorded at the moment. "\
+                "Please start recording before adding frames.")
+        self._send_request("add_frame")
+
+    def stop_and_save_video(self, path):
+        def file_available(path):
+            if not os.path.exists(path):
+                return False
+            for proc in psutil.process_iter():
+                try:
+                    if 'chrome' in proc.name():
+                        for item in proc.open_files():
+                            if path == item.path:
+                                return False
+                except psutil.NoSuchProcess:
+                    # The process ended before examining its files
+                    pass
+            return True
+
+        if not self.is_recording:
+            raise RuntimeError(
+                "No video being recorded at the moment. "\
+                "Please start recording and add frames before saving.")
+        path = os.path.abspath(pathlib.Path(path).with_suffix('.webm'))
+        if os.path.exists(path):
+            os.remove(path)
+        self._send_request("stop_and_save_record", message=path)
+        self.is_recording = False
+        while not file_available(path):
+            pass
---
+++ python/jiminy_py/src/jiminy_py/meshcat/wrapper.py
@@ -0,0 +1,43 @@
+from contextlib import redirect_stdout
+
+import meshcat
+
+from .recorder import MeshcatRecorder
+
+
+class MeshcatWrapper:
+    def __init__(self, zmq_url):
+        with redirect_stdout(None):
+            self.gui = meshcat.Visualizer(zmq_url)
+        self.__zmq_socket = self.gui.window.zmq_socket
+        self.recorder = MeshcatRecorder(self.gui.url())
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        self.recorder.release()
+
+    def wait(self, require_client=False):
+        if require_client:
+            self.gui.wait()
+        self.__zmq_socket.send(b"ready")
+        self.__zmq_socket.recv().decode("utf-8")
+
+    def start_recording(self, fps, width, height):
+        if not self.recorder.is_open:
+            self.recorder.open()
+            self.wait(require_client=True)
+        self.recorder.start_video_recording(fps, width, height)
+
+    def stop_recording(self, path):
+        self.recorder.stop_and_save_video(path)
+
+    def add_frame(self):
+        self.recorder.add_video_frame()
+
+    def capture_frame(self, width=None, height=None):
+        if not self.recorder.is_open:
+            self.recorder.open()
+            self.wait(require_client=True)
+        return self.recorder.capture_frame(width, height)
