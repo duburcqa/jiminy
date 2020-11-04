1923d14e626727b648d91908dbad0c501fc8b66f
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2020-11-04 18:14:09 +0100

    [misc] Fix Pinocchio imported twise that was messing up with boost python converters on Windows.

--- python/jiminy_py/src/jiminy_py/__init__.py
+++ python/jiminy_py/src/jiminy_py/__init__.py
@@ -3,35 +3,30 @@ import sys as _sys
 import importlib as _importlib
 from contextlib import redirect_stderr as _redirect_stderr
 
+# Import Pinocchio and co (use the embedded version only if necessary)
+if _importlib.util.find_spec("eigenpy") is not None:
+    import eigenpy as _eigenpy
+else:
+    from . import eigenpy as _eigenpy
+    _sys.modules["eigenpy"] = _eigenpy
+
+if _importlib.util.find_spec("hppfcl") is not None:
+    import hppfcl as _hppfcl
+else:
+    from . import hppfcl as _hppfcl
+    _sys.modules["hppfcl"] = _hppfcl
+
+if _importlib.util.find_spec("pinocchio") is not None:
+    import pinocchio as _pinocchio
+else:
+    from . import pinocchio as _pinocchio
+    _sys.modules["pinocchio"] = _pinocchio
+
+    # Register pinocchio_pywrap to avoid importing bindings twise, which messes 
+    #up with boost python converters.
+    _sys.modules["pinocchio.pinocchio_pywrap"] = _pinocchio.pinocchio_pywrap
+
 with open(_os.devnull, 'w') as stderr, _redirect_stderr(stderr):
-    # Import Pinocchio and co (use the embedded version only if necessary)
-    if _importlib.util.find_spec("eigenpy") is not None:
-        import eigenpy as _eigenpy
-    else:
-        from . import eigenpy as _eigenpy
-        _sys.modules["eigenpy"] = _eigenpy
-
-    if _importlib.util.find_spec("hppfcl") is not None:
-        import hppfcl as _hppfcl
-    else:
-        from . import hppfcl as _hppfcl
-        _sys.modules["hppfcl"] = _hppfcl
-
-    if _importlib.util.find_spec("pinocchio") is not None:
-        import pinocchio as _pinocchio
-    else:
-        from . import pinocchio as _pinocchio
-        _sys.modules["pinocchio"] = _pinocchio
-
-        # Mock 'StdVec_StdString' converter for Windows
-        if _os.name == 'nt':
-            _pinocchio.StdVec_StdString = list
-            _pinocchio.pinocchio_pywrap.StdVec_StdString = list
-
-    # Preload rpy module to avoid boost python warnings at import
-    from pinocchio.pinocchio_pywrap import rpy as _rpy
-    _sys.modules["pinocchio.pinocchio_pywrap.rpy"] = _rpy
-
     # Import core submodule once every dependency has been preloaded
     from . import core
     from .core import __version__, __raw_version__
