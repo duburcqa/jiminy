04101f1e13e77bb5c91de969e14f38fe0d6284bf
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2021-04-04 15:17:54 +0200

    [python/viewer] Use 'av' package instead of 'cv2' to export video. It is faster and support web-compatible 'h264' codec.

--- python/jiminy_py/src/jiminy_py/viewer/replay.py
+++ python/jiminy_py/src/jiminy_py/viewer/replay.py
@@ -1,6 +1,3 @@
-import io
-import os
-import sys
 import pathlib
 import asyncio
 import tempfile
@@ -9,7 +6,7 @@ from threading import Thread, Lock
 from itertools import cycle, islice
 from typing import Optional, Union, Sequence, Tuple, Dict, Any
 
-import cv2
+import av
 import numpy as np
 from tqdm import tqdm
 from typing_extensions import TypedDict
@@ -141,9 +138,8 @@ def play_trajectories(trajectory_data: Union[
     :param record_video_path: Fullpath location where to save generated video.
                               It must be specified to enable video recording.
                               Meshcat only support 'webm' format, while the
-                              other renderer only supports 'mp4'. 'mp4' video
-                              are very fast to record but not web-compatible
-                              because encoded using codec 'mp4v'.
+                              other renderer only supports 'mp4' format encoded
+                              with web-compatible 'h264' codec.
                               Optional: None by default.
     :param viewers: List of already instantiated viewers, associated one by one
                     in order to each trajectory data. None to disable.
@@ -380,48 +376,42 @@ def play_trajectories(trajectory_data: Union[
                         positions[i], xyz_offset=offset)
             if Viewer.backend == 'meshcat':
                 if not is_initialized:
+                    # Sanitize the recording path to enforce '.webm' extension
+                    record_video_path = str(
+                        pathlib.Path(record_video_path).with_suffix('.webm'))
+
+                    # Start backend recording thread
                     viewers[0]._backend_obj.start_recording(
                         VIDEO_FRAMERATE, *VIDEO_SIZE)
                 viewers[0]._backend_obj.add_frame()
             else:
                 frame = viewers[0].capture_frame(VIDEO_SIZE[1], VIDEO_SIZE[0])
                 if not is_initialized:
-                    # Determine the right video container and codec to use
-                    if pathlib.Path(record_video_path).suffix == ".webm":
-                        codec = cv2.VideoWriter_fourcc(*'VP80')
-                    else:  # fallback to mp4 container in any other case
-                        codec = cv2.VideoWriter_fourcc(*'mp4v')
-                        record_video_path = str(pathlib.Path(
-                            record_video_path).with_suffix('.mp4'))
+                    # Sanitize the recording path to enforce '.mp4' extension
+                    record_video_path = str(
+                        pathlib.Path(record_video_path).with_suffix('.mp4'))
 
-                    # Redirect opencv warnings
-                    original_stderr_fd = sys.stderr.fileno()
-                    saved_stderr_fd = os.dup(original_stderr_fd)
-                    with open(os.devnull, 'w') as tfile:
-                        try:
-                            sys.stderr.close()
-                            os.dup2(tfile.fileno(), original_stderr_fd)
-                            out = cv2.VideoWriter(
-                                record_video_path, codec, fps=VIDEO_FRAMERATE,
-                                frameSize=frame.shape[1::-1])
-                            os.dup2(saved_stderr_fd, original_stderr_fd)
-                            sys.stderr = io.TextIOWrapper(
-                                os.fdopen(original_stderr_fd, 'wb'))
-                        finally:
-                            os.close(saved_stderr_fd)
+                    # Create new video
+                    out = av.open(record_video_path, 'w')
+                    stream = out.add_stream('h264', str(VIDEO_FRAMERATE))
+                    stream.height, stream.width = VIDEO_SIZE
+                    stream.pix_fmt = 'yuv420p'
 
+                # Update video clock if enabled
                 if enable_clock and Viewer.backend == 'panda3d':
                     Viewer.set_clock(t_cur)
 
                 # Write frame
-                out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+                frame = av.VideoFrame.from_ndarray(frame, format='rgb24')
+                out.mux(stream.encode(frame))
             is_initialized = True
         if Viewer.backend == 'meshcat':
-            record_video_path = str(
-                pathlib.Path(record_video_path).with_suffix('.webm'))
+            # Stop backend recording thread
             viewers[0]._backend_obj.stop_recording(record_video_path)
         else:
-            out.release()
+            # Flush and close recording file
+            out.mux(stream.encode(None))
+            out.close()
     else:
         def replay_thread(viewer, *args):
             loop = asyncio.new_event_loop()
