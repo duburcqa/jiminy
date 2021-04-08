cde801c3cf0bd2f744817751bd871b9d7f0151e1
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2021-04-08 19:35:08 +0200

    [misc] Fix inter-operability with custom eigenpy/hppfcl/pinocchio install.

--- python/jiminy_py/src/jiminy_py/__init__.py
+++ python/jiminy_py/src/jiminy_py/__init__.py
@@ -6,6 +6,20 @@ from pkg_resources import parse_version as _version
 from contextlib import redirect_stderr as _redirect_stderr
 
 
+# Special dlopen flags are used when loading Boost Python shared library to
+# make sure the same boost python runtime is shared between every modules,
+# even if linked versions are different. It is necessary to share the same
+# boost python registers, required for inter-operability between modules.
+import ctypes as _ctypes
+from distutils.sysconfig import get_config_var as _get_config_var
+try:
+    python_ver_suffix = "".join(map(str, _sys.version_info[:2]))
+    shared_lib_suffix = _get_config_var('SHLIB_SUFFIX')
+    boost_python_lib = f"libboost_python{python_ver_suffix}{shared_lib_suffix}"
+    _ctypes.CDLL(boost_python_lib, _ctypes.RTLD_GLOBAL)
+except OSError:
+    pass
+
 # Fix Dll seach path on windows for Python >= 3.8
 if _sys.platform.startswith('win') and _sys.version_info >= (3, 8):
     for path in _os.environ['PATH'].split(_os.pathsep):
@@ -31,9 +45,9 @@ else:
     from . import pinocchio as _pinocchio
     _sys.modules["pinocchio"] = _pinocchio
 
-# Register pinocchio_pywrap to avoid importing bindings twise, which messes
-# up with boost python converters. In addition, submodules search path needs
-# to be fixed for releases older than 2.5.6.
+# Register pinocchio_pywrap to avoid importing bindings twise, which messes up
+# with boost python converters. In addition, submodules search path needs to be
+# fixed for releases older than 2.5.6.
 submodules = _inspect.getmembers(
     _pinocchio.pinocchio_pywrap, _inspect.ismodule)
 for module_name, module_obj in submodules:
@@ -48,10 +62,9 @@ with open(_os.devnull, 'w') as stderr, _redirect_stderr(stderr):
     from . import core
     from .core import __version__, __raw_version__
 
-# Patch Pinocchio to avoid loading ground geometry in viewer, and
-# force numpy.ndarray type for from/to Python matrix converters.
+# Patch Pinocchio to avoid loading ground geometry in viewer, and force
+# `np.ndarray` type for from/to Python matrix converters.
 from . import _pinocchio_init as _patch  # noqa
-
 
 __all__ = [
     'core',
