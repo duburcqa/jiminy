fb44fa24a7835d12888e464319d90a2402590657
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2021-04-17 15:05:02 +0200

    [misc] Refactor legacy CMake Python finding to be consistent with CMake>=3.12.

--- build_tools/cmake/FindNumPy.cmake
+++ build_tools/cmake/FindNumPy.cmake
@@ -1,34 +1,34 @@
 # Find the Python NumPy package
-# NUMPY_INCLUDE_DIRS
-# NUMPY_FOUND
+# NumPy_INCLUDE_DIRS
+# NumPy_FOUND
 # will be set by this script
 
 cmake_minimum_required(VERSION 3.10)
 
-if(PYTHON_EXECUTABLE)
+if(Python_EXECUTABLE)
     # Find out the include path
     execute_process(
-        COMMAND "${PYTHON_EXECUTABLE}" -c
-        "from __future__ import print_function\ntry: import numpy; print(numpy.get_include(), end='')\nexcept:pass\n"
+        COMMAND "${Python_EXECUTABLE}" -c
+        "try: import numpy; print(numpy.get_include(), end='')\nexcept:pass\n"
         OUTPUT_VARIABLE __numpy_path)
     # And the version
     execute_process(
-        COMMAND "${PYTHON_EXECUTABLE}" -c
-        "from __future__ import print_function\ntry: import numpy; print(numpy.__version__, end='')\nexcept:pass\n"
+        COMMAND "${Python_EXECUTABLE}" -c
+        "try: import numpy; print(numpy.__version__, end='')\nexcept:pass\n"
         OUTPUT_VARIABLE __numpy_version)
 else()
     message(FATAL_ERROR "Python executable not found.")
 endif()
 
-unset(NUMPY_INCLUDE_DIRS)
-unset(NUMPY_INCLUDE_DIRS CACHE)
-find_path(NUMPY_INCLUDE_DIRS numpy/arrayobject.h
-    HINTS "${__numpy_path}" "${PYTHON_INCLUDE_DIRS}" NO_DEFAULT_PATH)
+unset(NumPy_INCLUDE_DIRS)
+unset(NumPy_INCLUDE_DIRS CACHE)
+find_path(NumPy_INCLUDE_DIRS numpy/arrayobject.h
+    HINTS "${__numpy_path}" "${Python_INCLUDE_DIRS}" NO_DEFAULT_PATH)
 
-if(NUMPY_INCLUDE_DIRS)
-    set(NUMPY_FOUND 1 CACHE INTERNAL "Python numpy found")
+if(NumPy_INCLUDE_DIRS)
+    set(NumPy_FOUND 1 CACHE INTERNAL "Python numpy found")
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NumPy REQUIRED_VARS NUMPY_INCLUDE_DIRS
+find_package_handle_standard_args(NumPy REQUIRED_VARS NumPy_INCLUDE_DIRS
     VERSION_VAR __numpy_version)
--- build_tools/cmake/setupPython.cmake
+++ build_tools/cmake/setupPython.cmake
@@ -2,55 +2,49 @@
 
 # Get the required information to build Python bindings
 
-# Get Python executable and version
-if(NOT DEFINED PYTHON_EXECUTABLE)
+# Get Python executable
+if(DEFINED PYTHON_EXECUTABLE)
+    get_filename_component(_PYTHON_PATH "${PYTHON_EXECUTABLE}" DIRECTORY)
+    get_filename_component(_PYTHON_NAME "${PYTHON_EXECUTABLE}" NAME)
+    find_program(Python_EXECUTABLE "${_PYTHON_NAME}" PATHS "${_PYTHON_PATH}" NO_DEFAULT_PATH)
+else()
     if(CMAKE_VERSION VERSION_LESS "3.12.4")
-        if(PYTHON_REQUIRED_VERSION)
-            message(FATAL_ERROR "Impossible to handle PYTHON_REQUIRED_VERSION for cmake older than 3.12.4, Cmake will exit.")
-        endif()
-        find_program(PYTHON_EXECUTABLE python)
-        if(NOT PYTHON_EXECUTABLE)
-            message(FATAL_ERROR "No Python executable found, CMake will exit.")
-        endif()
+        find_program(Python_EXECUTABLE "python${PYTHON_REQUIRED_VERSION}")
     else()
         if(PYTHON_REQUIRED_VERSION)
-            find_package(Python ${PYTHON_REQUIRED_VERSION} EXACT REQUIRED COMPONENTS Interpreter)
+            find_package(Python ${PYTHON_REQUIRED_VERSION} EXACT COMPONENTS Interpreter)
         else()
-            find_package(Python REQUIRED COMPONENTS Interpreter)
+            find_package(Python COMPONENTS Interpreter)
         endif()
-        set(PYTHON_EXECUTABLE "${Python_EXECUTABLE}")
-    endif()
-else()
-    find_program(PYTHON_EXECUTABLE python PATHS "${PYTHON_EXECUTABLE}" NO_DEFAULT_PATH)
-    if(NOT PYTHON_EXECUTABLE)
-        message(FATAL_ERROR "User-specified Python executable not valid, CMake will exit.")
     endif()
 endif()
+if(NOT Python_EXECUTABLE)
+    message(FATAL_ERROR "Python executable not found, CMake will exit.")
+endif()
 
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                        "import sys; sys.stdout.write(';'.join([str(x) for x in sys.version_info[:3]]))"
-                OUTPUT_STRIP_TRAILING_WHITESPACE
+# Get Python version
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                        "import sys ; print(';'.join([str(x) for x in sys.version_info[:3]]), end='')"
                 OUTPUT_VARIABLE _VERSION)
-list(GET _VERSION 0 PYTHON_VERSION_MAJOR)
-list(GET _VERSION 1 PYTHON_VERSION_MINOR)
-list(GET _VERSION 2 PYTHON_VERSION_PATCH)
-set(PYTHON_VERSION "${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}")
-if(PYTHON_VERSION_MAJOR EQUAL 3)
-    message(STATUS "Found PythonInterp: ${PYTHON_EXECUTABLE} (found version \"${PYTHON_VERSION_STRING}\")")
+list(GET _VERSION 0 Python_VERSION_MAJOR)
+list(GET _VERSION 1 Python_VERSION_MINOR)
+list(GET _VERSION 2 Python_VERSION_PATCH)
+set(Python_VERSION "${Python_VERSION_MAJOR}.${Python_VERSION_MINOR}")
+if(Python_VERSION_MAJOR EQUAL 3)
+    message(STATUS "Found PythonInterp: ${Python_EXECUTABLE} (found version \"${Python_VERSION}\")")
 else()
     message(FATAL_ERROR "Python3 is required if BUILD_PYTHON_INTERFACE=ON.")
 endif()
 
 # Get Python system and user site-packages
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                        "import sysconfig; print(sysconfig.get_paths()['purelib'])"
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                        "import sysconfig; print(sysconfig.get_paths()['purelib'], end='')"
+                OUTPUT_VARIABLE Python_SYS_SITELIB)
+message(STATUS "Python system site-packages: ${Python_SYS_SITELIB}")
+execute_process(COMMAND "${Python_EXECUTABLE}" -m site --user-site
                 OUTPUT_STRIP_TRAILING_WHITESPACE
-                OUTPUT_VARIABLE PYTHON_SYS_SITELIB)
-message(STATUS "Python system site-packages: ${PYTHON_SYS_SITELIB}")
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" -m site --user-site
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                OUTPUT_VARIABLE PYTHON_USER_SITELIB)
-message(STATUS "Python user site-package: ${PYTHON_USER_SITELIB}")
+                OUTPUT_VARIABLE Python_USER_SITELIB)
+message(STATUS "Python user site-package: ${Python_USER_SITELIB}")
 
 # Check write permissions on Python system site-package to
 # determine whether to use user site as fallback.
@@ -59,25 +53,24 @@ if(WIN32)
     set(HAS_NO_WRITE_PERMISSION_ON_PYTHON_SYS_SITELIB FALSE)
 else()
     execute_process(COMMAND bash -c
-                            "if test -w ${PYTHON_SYS_SITELIB} ; then echo 0; else echo 1; fi"
+                    "if test -w ${Python_SYS_SITELIB} ; then echo 0; else echo 1; fi"
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     OUTPUT_VARIABLE HAS_NO_WRITE_PERMISSION_ON_PYTHON_SYS_SITELIB)
 endif()
 
 if(${HAS_NO_WRITE_PERMISSION_ON_PYTHON_SYS_SITELIB})
     set(PYTHON_INSTALL_FLAGS " --user ")
-    set(PYTHON_SITELIB "${PYTHON_USER_SITELIB}")
-    message(STATUS "No right on Python system site-packages: ${PYTHON_SYS_SITELIB}.\n"
+    set(Python_SITELIB "${Python_USER_SITELIB}")
+    message(STATUS "No right on Python system site-packages: ${Python_SYS_SITELIB}.\n"
                    "--   Installing on user site as fallback.")
 else()
-    set(PYTHON_SITELIB "${PYTHON_SYS_SITELIB}")
+    set(Python_SITELIB "${Python_SYS_SITELIB}")
 endif()
 
 # Get PYTHON_EXT_SUFFIX
 set(PYTHON_EXT_SUFFIX "")
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                        "from distutils.sysconfig import get_config_var; print(get_config_var('EXT_SUFFIX'))"
-                OUTPUT_STRIP_TRAILING_WHITESPACE
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                        "from distutils.sysconfig import get_config_var; print(get_config_var('EXT_SUFFIX'), end='')"
                 OUTPUT_VARIABLE PYTHON_EXT_SUFFIX)
 if("${PYTHON_EXT_SUFFIX}" STREQUAL "")
     if(WIN32)
@@ -88,20 +81,19 @@ if("${PYTHON_EXT_SUFFIX}" STREQUAL "")
 ENDIF()
 
 # Include Python headers
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-                        "import distutils.sysconfig as sysconfig; print(sysconfig.get_python_inc())"
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                OUTPUT_VARIABLE PYTHON_INCLUDE_DIRS)
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                        "import distutils.sysconfig as sysconfig; print(sysconfig.get_python_inc(), end='')"
+                OUTPUT_VARIABLE Python_INCLUDE_DIRS)
 
 # Add Python library directory to search path on Windows
 if(WIN32)
-    get_filename_component(PYTHON_ROOT ${PYTHON_SYS_SITELIB} DIRECTORY)
+    get_filename_component(PYTHON_ROOT ${Python_SYS_SITELIB} DIRECTORY)
     get_filename_component(PYTHON_ROOT ${PYTHON_ROOT} DIRECTORY)
     link_directories("${PYTHON_ROOT}/libs/")
     message(STATUS "Found PythonLibraryDirs: ${PYTHON_ROOT}/libs/")
 endif()
 
-# Define NUMPY_INCLUDE_DIRS
+# Define NumPy_INCLUDE_DIRS
 find_package(NumPy REQUIRED)
 
 # Define BOOST_PYTHON_LIB
@@ -113,8 +105,8 @@ if(${Boost_MINOR_VERSION} GREATER_EQUAL 67)
     set(Boost_LIB_PREFIX "")
     unset(Boost_LIBRARIES)
     find_package(Boost REQUIRED COMPONENTS
-                "python${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}"
-                "numpy${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}")
+                "python${Python_VERSION_MAJOR}${Python_VERSION_MINOR}"
+                "numpy${Python_VERSION_MAJOR}${Python_VERSION_MINOR}")
     set(BOOST_PYTHON_LIB "${Boost_LIBRARIES}")
     unset(Boost_LIBRARIES)
     if(WIN32)
@@ -130,7 +122,7 @@ message(STATUS "Boost Python Libs: ${BOOST_PYTHON_LIB}")
 function(deployPythonPackage)
     # The input arguments are [TARGET_NAME...]
     foreach(TARGET_NAME IN LISTS ARGN)
-        install(CODE "execute_process(COMMAND ${PYTHON_EXECUTABLE} -m pip install ${PYTHON_INSTALL_FLAGS} --upgrade .
+        install(CODE "execute_process(COMMAND ${Python_EXECUTABLE} -m pip install ${PYTHON_INSTALL_FLAGS} --upgrade .
                                       WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/pypi/${TARGET_NAME})")
     endforeach()
 endfunction()
@@ -138,7 +130,7 @@ endfunction()
 function(deployPythonPackageDevelop)
     # The input arguments are [TARGET_NAME...]
     foreach(TARGET_NAME IN LISTS ARGN)
-        install(CODE "execute_process(COMMAND ${PYTHON_EXECUTABLE} -m pip install ${PYTHON_INSTALL_FLAGS} -e .
+        install(CODE "execute_process(COMMAND ${Python_EXECUTABLE} -m pip install ${PYTHON_INSTALL_FLAGS} -e .
                                       WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/${TARGET_NAME})")
     endforeach()
 endfunction()
