f2bca87e62ffcd195c194f88e118e6a325090c01
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2021-07-26 17:10:43 +0200

    [gym/toolbox] Fix computation of distance from convex hull.

--- python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/__init__.py
+++ python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 
 from .spline import Spline
-from .qhull import ConvexHull
+from .qhull import ConvexHull, compute_distance_convex_to_point
 from .generic import squared_norm_2
 from .signal import integrate_zoh
 
@@ -9,6 +9,7 @@ from .signal import integrate_zoh
 __all__ = [
     "Spline",
     "ConvexHull",
+    "compute_distance_convex_to_point",
     "squared_norm_2",
     "integrate_zoh"
 ]
