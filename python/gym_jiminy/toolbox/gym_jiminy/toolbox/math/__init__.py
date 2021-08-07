72c607a9ed2c15605380ab7068ee345d57f4f89e
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2021-08-07 11:26:19 +0200

    [gym/toolbox] Add 'quat_to_yaw_cos_sin' math helper.

--- python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/__init__.py
+++ python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/__init__.py
@@ -2,7 +2,10 @@
 
 from .spline import Spline
 from .qhull import ConvexHull, compute_distance_convex_to_point
-from .generic import squared_norm_2, matrix_to_yaw, quat_to_yaw
+from .generic import (squared_norm_2,
+                      matrix_to_yaw,
+                      quat_to_yaw_cos_sin,
+                      quat_to_yaw)
 from .signal import integrate_zoh
 
 
@@ -12,6 +15,7 @@ __all__ = [
     "compute_distance_convex_to_point",
     "squared_norm_2",
     "matrix_to_yaw",
+    "quat_to_yaw_cos_sin",
     "quat_to_yaw",
     "integrate_zoh"
 ]
--- python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/generic.py
+++ python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/generic.py
@@ -1,6 +1,7 @@
 """ TODO: Write documentation.
 """
 import math
+from typing import Tuple
 
 import numba as nb
 import numpy as np
@@ -16,17 +17,27 @@ def squared_norm_2(array: np.ndarray) -> float:
 
 @nb.jit(nopython=True, nogil=True)
 def matrix_to_yaw(rotation_matrix: np.ndarray) -> float:
-    """Compute the yaw from Yaw-Pitch-Roll Euler angles representation from a
+    """Compute the yaw from Yaw-Pitch-Roll Euler angles representation of a
     rotation matrix.
     """
     return math.atan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
 
 
 @nb.jit(nopython=True, nogil=True)
+def quat_to_yaw_cos_sin(quaternion: np.ndarray) -> Tuple[float, float]:
+    """Compute the cosinus and sinus of the yaw from Yaw-Pitch-Roll Euler
+    angles representation of a quaternion.
+    """
+    cos_yaw = - 1.0 + 2.0 * (quaternion[3] ** 2 + quaternion[0] ** 2)
+    sin_yaw = 2.0 * (
+        quaternion[2] * quaternion[3] + quaternion[0] * quaternion[1])
+    return cos_yaw, sin_yaw
+
+
+@nb.jit(nopython=True, nogil=True)
 def quat_to_yaw(quaternion: np.ndarray) -> float:
-    """Compute the yaw from Yaw-Pitch-Roll Euler angles representation from a
+    """Compute the yaw from Yaw-Pitch-Roll Euler angles representation of a
     quaternion.
     """
-    return math.atan2(
-        2.0 * (quaternion[2] * quaternion[3] + quaternion[0] * quaternion[1]),
-        - 1.0 + 2.0 * (quaternion[3] ** 2 + quaternion[0] ** 2))
+    cos_yaw, sin_yaw = quat_to_yaw_cos_sin(quaternion)
+    return math.atan2(sin_yaw, cos_yaw)
