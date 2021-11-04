d5f3226b24b2b262b7501f84ef123e062a5bc8b3
Alexis Duburcq <alexis.duburcq@wandercraft.eu>
2021-11-04 14:09:51 +0100

    [gym/rllib] Add option to forward keyword arguments to 'ray.init'.

--- core/src/engine/EngineMultiRobot.cc
+++ core/src/engine/EngineMultiRobot.cc
@@ -3789,7 +3789,7 @@ namespace jiminy
             // Update lagrangian multipliers associated with the constraint
             constraintIdx = 0U;
             systemData.constraintsHolder.foreach(
-                [&lambda_c = const_cast<vectorN_t &>(data.lambda_c),  // std::as_const is not supported by gcc<7.3
+                [&lambda_c = const_cast<vectorN_t const &>(data.lambda_c),  // std::as_const is not supported by gcc<7.3
                  &constraintIdx](
                     std::shared_ptr<AbstractConstraintBase> const & constraint,
                     constraintsHolderType_t const & /* holderType */)
@@ -3808,7 +3808,7 @@ namespace jiminy
                 constraintsHolderType_t::BOUNDS_JOINTS,
                 [&u = systemData.state.u,
                  &uInternal = systemData.state.uInternal,
-                 &joints = const_cast<pinocchio::Model::JointModelVector &>(model.joints)](
+                 &joints = const_cast<pinocchio::Model::JointModelVector const &>(model.joints)](
                     std::shared_ptr<AbstractConstraintBase> & constraint,
                     constraintsHolderType_t const & /* holderType */)
                 {
