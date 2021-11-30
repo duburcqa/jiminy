[core] Fix PGS cone bounds computation. (#460)
Co-authored-by: Alexis Duburcq <alexis.duburcq@wandercraft.eu>

python/gym_jiminy/common/gym_jiminy/common/envs/env_generic.py
@@ -809,7 +809,7 @@ class BaseJiminyEnv(ObserverControllerInterface, gym.Env):
                 reward += self.compute_reward_terminal(info=self._info)
 
         # Make sure the reward is not 'nan'
-        if reward != reward:
+        if np.isnan(reward):
             raise RuntimeError(
                 "The reward is 'nan'. Something went wrong with "
                 "`compute_reward` or `compute_reward_terminal` methods.")