34b711214f29bd9d4322194b911ccbd143e65409
Alexis Duburcq <alexis.duburcq@gmail.com>
2023-08-20 19:28:17 +0200

    [gym/toolbox] More orientation conversion utils. Move components to first dim.

--- python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/generic.py
+++ python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/generic.py
@@ -1,13 +1,12 @@
 """ TODO: Write documentation.
 """
-import math
 from typing import Union
 
 import numpy as np
 import numba as nb
 
 
-@nb.jit(nopython=True, cache=True, inline='always')
+@nb.jit(nopython=True, cache=True)
 def squared_norm_2(array: np.ndarray) -> float:
     """Fast implementation of the sum of squared array elements, optimized for
     small to medium size 1D arrays.
@@ -15,62 +14,147 @@ def squared_norm_2(array: np.ndarray) -> float:
     return np.sum(np.square(array))
 
 
-@nb.jit(nopython=True, nogil=True, cache=True, inline='always')
-def matrix_to_yaw(rotation_matrix: np.ndarray) -> float:
+@nb.jit(nopython=True, cache=True)
+def matrix_to_yaw(mat: np.ndarray) -> float:
     """Compute the yaw from Yaw-Pitch-Roll Euler angles representation of a
-    rotation matrix.
+    rotation matrix in 3D Euclidean space.
+
+    :param mat: N-dimensional array whose first and second dimensions gathers
+                the 3-by-3 rotation matrix elements.
     """
-    return math.atan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
+    assert mat.ndim >= 2
+    return np.arctan2(mat[1, 0], mat[0, 0])
 
 
-@nb.jit(nopython=True, nogil=True, cache=True, inline='always')
+@nb.jit(nopython=True, cache=True)
 def quat_to_yaw_cos_sin(quat: np.ndarray) -> np.ndarray:
     """Compute cosine and sine of the yaw from Yaw-Pitch-Roll Euler angles
     representation of a single or a batch of quaternions.
 
-    :param quat: 1D array or a 2D array whose first dimension corresponds to
-                 the number of individual quaternions, and the second to the 4
-                 coordinates [qx, qy, qz, qw].
+    :param quat: N-dimensional array whose first dimension gathers the 4
+                 quaternion coordinates [qx, qy, qz, qw].
     """
-    qx, qy, qz, qw = np.atleast_2d(quat)[:, -4:].T
-    cos_yaw = 2 * (qw * qw + qx * qx) - 1.0
-    sin_yaw = 2 * (qw * qz + qx * qy)
-    yaw_cos_sin = np.stack((cos_yaw, sin_yaw), axis=-1)
+    assert quat.ndim >= 1
+    (qxy, qyy), (qzz, qzw) = quat[-3] * quat[-4:-2], quat[-2] * quat[-2:]
+    cos_yaw, sin_yaw = 1.0 - 2 * (qyy + qzz), 2 * (qxy + qzw)
     if quat.ndim == 1:
-        return yaw_cos_sin[0]
-    return yaw_cos_sin
+        return np.array((cos_yaw, sin_yaw))
+    return np.stack((cos_yaw, sin_yaw))
 
 
-@nb.jit(nopython=True, nogil=True, cache=True, inline='always')
+@nb.jit(nopython=True, cache=True)
 def quat_to_yaw(quat: np.ndarray) -> Union[float, np.ndarray]:
     """Compute the yaw from Yaw-Pitch-Roll Euler angles representation of a
     single or a batch of quaternions.
 
-    :param quat: 1D array or a 2D array whose first dimension corresponds to
-                 the number of individual quaternions, and the second to the 4
-                 coordinates [qx, qy, qz, qw].
+    :param quat: N-dimensional array whose first dimension gathers the 4
+                 quaternion coordinates [qx, qy, qz, qw].
     """
-    cos_yaw, sin_yaw = quat_to_yaw_cos_sin(quat).T
+    assert quat.ndim >= 1
+    cos_yaw, sin_yaw = quat_to_yaw_cos_sin(quat)
     return np.arctan2(sin_yaw, cos_yaw)
 
 
-@nb.jit(nopython=True, nogil=True, cache=True, inline='always')
+@nb.jit(nopython=True, cache=True)
 def quat_to_rpy(quat: np.ndarray) -> np.ndarray:
     """Compute the Yaw-Pitch-Roll Euler angles representation of a single or a
     batch of quaternions.
 
-    :param quat: 1D array or a 2D array whose first dimension corresponds to
-                 the number of individual quaternions, and the second to the 4
-                 coordinates [qx, qy, qz, qw].
+    :param quat: N-dimensional array whose first dimension gathers the 4
+                 quaternion coordinates [qx, qy, qz, qw].
     """
-    qx, qy, qz, qw = np.atleast_2d(quat)[:, -4:].T
-    roll = np.arctan2(2 * (qw * qx + qy * qz), 1.0 - 2 * (qx * qx + qy * qy))
+    assert quat.ndim >= 1
+    qxx, qxy, qxz, qxw = quat[-4] * quat[-4:]
+    qyy, qyz, qyw = quat[-3] * quat[-3:]
+    qzz, qzw = quat[-2] * quat[-2:]
+    roll = np.arctan2(2 * (qxw + qyz), 1.0 - 2 * (qxx + qyy))
     pitch = -np.pi / 2 + 2 * np.arctan2(
-        np.sqrt(1 + 2 * (qw * qy - qx * qz)),
-        np.sqrt(1 - 2 * (qw * qy - qx * qz)),
+        np.sqrt(1.0 + 2 * (qyw - qxz)),
+        np.sqrt(1.0 - 2 * (qyw - qxz)),
     )
-    yaw = np.arctan2(2 * (qw * qz + qx * qy), 1.0 - 2 * (qy * qy + qz * qz))
-    rpy = np.stack((roll, pitch, yaw), axis=-1)
+    yaw = np.arctan2(2 * (qzw + qxy), 1.0 - 2 * (qyy + qzz))
     if quat.ndim == 1:
-        return rpy[0]
-    return rpy
+        return np.array((roll, pitch, yaw))
+    return np.stack((roll, pitch, yaw))
+
+
+@nb.jit(nopython=True, cache=True)
+def quat_to_matrix(quat: np.ndarray) -> np.ndarray:
+    """Compute the Rotation Matrix representation of a single or a
+    batch of quaternions.
+
+    :param quat: N-dimensional array whose first dimension gathers the 4
+                 quaternion coordinates [qx, qy, qz, qw].
+    """
+    assert quat.ndim >= 1
+    qxx, qxy, qxz, qxw = quat[-4] * quat[-4:]
+    qyy, qyz, qyw = quat[-3] * quat[-3:]
+    qzz, qzw = quat[-2] * quat[-2:]
+    mat_flat = (
+        1.0 - 2 * (qyy + qzz), 2 * (qxy - qzw), 2 * (qxz + qyw),
+        2 * (qxy + qzw), 1.0 - 2 * (qxx + qzz), 2 * (qyz - qxw),
+        2 * (qxz - qyw), 2 * (qyz + qxw), 1.0 - 2 * (qxx + qyy),
+    )
+    if quat.ndim == 1:
+        return np.array(mat_flat).reshape((3, 3))
+    return np.stack(mat_flat).reshape((3, 3, *quat.shape[1:]))
+
+
+@nb.jit(nopython=True, cache=True)
+def matrix_to_quat(mat: np.ndarray) -> np.ndarray:
+    """Compute the [qx, qy, qz, qw] Quaternion representation of a single or a
+    batch of rotation matrices.
+
+    :param mat: N-dimensional array whose first and second dimensions gathers
+                the 3-by-3 rotation matrix elements.
+    """
+    assert mat.ndim >= 2
+    quat_flat = (
+        mat[2, 1] - mat[1, 2],
+        mat[0, 2] - mat[2, 0],
+        mat[1, 0] - mat[0, 1],
+        1.0 + mat[0, 0] + mat[1, 1] + mat[2, 2])
+    quat = np.array(quat_flat) if mat.ndim == 2 else np.stack(quat_flat)
+    quat /= np.sqrt(np.sum(quat * quat, 0))
+    return quat
+
+
+@nb.jit(nopython=True, cache=True)
+def rpy_to_matrix(rpy: np.ndarray) -> np.ndarray:
+    """Compute the Rotation Matrix representation of a single or a
+    batch of Yaw-Pitch-Roll Euler angles.
+
+    :param rpy: N-dimensional array whose first dimension gathers the 3
+                Yaw-Pitch-Roll Euler angles [Roll, Pitch, Yaw].
+    """
+    (c_r, c_p, c_y), (s_r, s_p, s_y) = np.cos(rpy[-3:]), np.sin(rpy[-3:])
+    mat_flat = (
+        c_p * c_y, -c_r * s_y + s_r * s_p * c_y,  s_r * s_y + c_r * s_p * c_y,
+        c_p * s_y,  c_r * c_y + s_r * s_p * s_y, -s_r * c_y + c_r * s_p * s_y,
+        -s_p, s_r * c_p, c_r * c_p
+    )
+    if rpy.ndim == 1:
+        return np.array(mat_flat).reshape((3, 3))
+    return np.stack(mat_flat).reshape((3, 3, *rpy.shape[1:]))
+
+
+@nb.jit(nopython=True, cache=True)
+def matrix_to_rpy(mat: np.ndarray) -> np.ndarray:
+    """Compute the Yaw-Pitch-Roll Euler angles representation of a single or a
+    batch of rotation matrices.
+
+    :param mat: N-dimensional array whose first and second dimensions gathers
+                the 3-by-3 rotation matrix elements.
+    """
+    return quat_to_rpy(matrix_to_quat(mat))
+
+
+@nb.jit(nopython=True, cache=True)
+def rpy_to_quat(rpy: np.ndarray) -> np.ndarray:
+    """Compute the [qx, qy, qz, qw] Quaternion representation of a single or a
+    batch of Yaw-Pitch-Roll Euler angles.
+
+    :param rpy: N-dimensional array whose first dimension gathers the 3
+                Yaw-Pitch-Roll Euler angles [Roll, Pitch, Yaw].
+    """
+    return matrix_to_quat(rpy_to_matrix(rpy))
