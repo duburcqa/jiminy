64e44422595a05b65cbfe3ff581b4fcd3e131474
mwulfman <mathias.wulfman@gmail.com>
2023-08-22 22:45:48 +0200

    :hammer: format the code and fix the conversion functions for ndim>=3

--- python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/generic.py
+++ python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/generic.py
@@ -70,7 +70,7 @@ def quat_to_rpy(quat: np.ndarray) -> np.ndarray:
         np.sqrt(1 - 2 * (qw * qy - qx * qz)),
     )
     yaw = np.arctan2(2 * (qw * qz + qx * qy), 1.0 - 2 * (qy * qy + qz * qz))
-    rpy = np.stack((roll, pitch, yaw), axis=-1)
+    rpy = np.stack((roll.T, pitch.T, yaw.T), axis=-1)
     if quat.ndim == 1:
         return rpy[0]
     return rpy
@@ -86,30 +86,21 @@ def quat_to_matrix(quat: np.ndarray) -> np.ndarray:
                  last to the 4 coordinates [qx, qy, qz, qw].
     """
     qx, qy, qz, qw = np.atleast_2d(quat)[..., -4:].T
-    col_1 = np.stack(
-        (
-            qx * qx - qy * qy - qz * qz + qw * qw,
-            2 * (qx * qy + qz * qw),
-            2 * (qx * qz - qy * qw),
-        ),
-        -1,
-    )
-    col_2 = np.stack(
-        (
-            2 * (qx * qy - qz * qw),
-            -qx * qx + qy * qy - qz * qz + qw * qw,
-            2 * (qy * qz + qx * qw),
-        ),
-        -1,
-    )
-    col_3 = np.stack(
-        (
-            2 * (qx * qz + qy * qw),
-            2 * (qy * qz - qx * qw),
-            -qx * qx - qy * qy + qz * qz + qw * qw,
-        ),
-        -1,
-    )
+    col_1 = np.stack((
+        (qx * qx - qy * qy - qz * qz + qw * qw).T,
+        (2 * (qx * qy + qz * qw)).T,
+        (2 * (qx * qz - qy * qw)).T
+        ), -1)
+    col_2 = np.stack((
+        (2 * (qx * qy - qz * qw)).T,
+        (-qx * qx + qy * qy - qz * qz + qw * qw).T,
+        (2 * (qy * qz + qx * qw)).T
+        ), -1)
+    col_3 = np.stack((
+        (2 * (qx * qz + qy * qw)).T,
+        (2 * (qy * qz - qx * qw)).T,
+        (-qx * qx - qy * qy + qz * qz + qw * qw).T
+        ), -1)
     matrix = np.stack((col_1, col_2, col_3), -1)
     if quat.ndim == 1:
         return matrix[0, :, :]
@@ -124,18 +115,23 @@ def matrix_to_quat(matrix: np.ndarray) -> np.ndarray:
     :param matrix: 2D array, a 3D array or a nD array whose first dimensions
                    corresponds to the number of individual rotation matrices,
                    and the two last to the 3-by-3 rotation matrices.
+    :warning: The function assumes unit rotation matrix.
     """
-    quat = np.stack(
-        (
-            matrix[..., 2, 1] - matrix[..., 1, 2],
-            matrix[..., 0, 2] - matrix[..., 2, 0],
-            matrix[..., 1, 0] - matrix[..., 0, 1],
-            1 + matrix[..., 0, 0] + matrix[..., 1, 1] + matrix[..., 2, 2],
-        ),
-        -1,
-    )
+    assert matrix.ndim >=2
+    if matrix.ndim==2:
+        mat = np.atleast_3d(matrix).T
+    else:
+        mat = matrix[:]
+    quat = np.stack((
+        mat[..., 2, 1] - mat[..., 1, 2],
+        mat[..., 0, 2] - mat[..., 2, 0],
+        mat[..., 1, 0] - mat[..., 0, 1],
+        1 + mat[..., 0, 0] + mat[..., 1, 1] + mat[..., 2, 2]
+        ), -1)
     norm = np.sqrt(np.sum(np.atleast_2d(quat * quat), -1))
     quat /= norm.reshape((*quat.shape[:-1], 1))
+    if matrix.ndim==2:
+        return quat[0]
     return quat
 
 
@@ -148,24 +144,25 @@ def rpy_to_matrix(rpy: np.ndarray) -> np.ndarray:
                 corresponds to the number of individual Euler angles, and the
                 last to the 3 coordinates [Roll, Pitch, Yaw].
     """
-    c_r, c_p, c_y = np.cos(np.atleast_2d(rpy)[..., -3:].T)
-    s_r, s_p, s_y = np.sin(np.atleast_2d(rpy)[..., -3:].T)
-    col_1 = np.stack((c_p * c_y, c_p * s_y, -s_p), -1)
-    col_2 = np.stack(
-        (-c_r * s_y + s_r * s_p * c_y, c_r * c_y + s_r * s_p * s_y, s_r * c_p),
-        -1,
-    )
-    col_3 = np.stack(
-        (s_r * s_y + c_r * s_p * c_y, -s_r * c_y + c_r * s_p * s_y, c_r * c_p),
-        -1,
-    )
+    c_r, c_p, c_y = np.cos(np.atleast_2d(rpy)[..., -3:]).T
+    s_r, s_p, s_y = np.sin(np.atleast_2d(rpy)[..., -3:]).T
+    col_1 = np.stack(((c_p * c_y).T, (c_p * s_y).T, -s_p.T), -1)
+    col_2 = np.stack((
+        (-c_r * s_y + s_r * s_p * c_y).T,
+        (c_r * c_y + s_r * s_p * s_y).T,
+        (s_r * c_p).T
+        ), -1)
+    col_3 = np.stack((
+        (s_r * s_y + c_r * s_p * c_y).T,
+        (-s_r * c_y + c_r * s_p * s_y).T,
+        (c_r * c_p).T
+        ), -1)
     matrix = np.stack((col_1, col_2, col_3), -1)
     if rpy.ndim == 1:
         return matrix[0, :, :]
     return matrix
 
 
-@nb.jit(nopython=True, nogil=True, inline='always')
 def matrix_to_rpy(matrix: np.ndarray) -> np.ndarray:
     """Compute the Yaw-Pitch-Roll Euler angles representation of a single or a
     batch of rotation matrices.
@@ -173,11 +170,11 @@ def matrix_to_rpy(matrix: np.ndarray) -> np.ndarray:
     :param matrix: 2D array, a 3D array or a nD array whose first dimensions
                    corresponds to the number of individual rotation matrices,
                    and the two last to the 3-by-3 rotation matrices.
+    :warning: The function assumes unit rotation matrix.
     """
     return quat_to_rpy(matrix_to_quat(matrix))
 
 
-@nb.jit(nopython=True, nogil=True, inline='always')
 def rpy_to_quat(rpy: np.ndarray) -> np.ndarray:
     """Compute the [qx, qy, qz, qw] Quaternion representation of a single or a
     batch of Yaw-Pitch-Roll Euler angles.
