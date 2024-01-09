d1ae183f16fa2df3fe2c91774d3bb1db7aaca2da
Alexis Duburcq <alexis.duburcq@gmail.com>
2024-01-09 14:01:29 +0100

    [misc] Add support of Python 3.12 for jiminy_py.

--- python/gym_jiminy/common/setup.py
+++ python/gym_jiminy/common/setup.py
@@ -31,7 +31,7 @@ setup(
                   "@PROJECT_VERSION@.tar.gz"),
     maintainer="Alexis Duburcq",
     license="MIT",
-    python_requires=">=3.8,<3.12",
+    python_requires=">=3.8,<3.13",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
@@ -41,7 +41,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ],
     keywords="reinforcement-learning robotics gym jiminy",
     packages=find_namespace_packages(),
--- python/gym_jiminy/toolbox/setup.py
+++ python/gym_jiminy/toolbox/setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="alexis.duburcq@gmail.com",
     maintainer="Alexis Duburcq",
     license="MIT",
-    python_requires=">=3.8,<3.12",
+    python_requires=">=3.8,<3.13",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
@@ -24,7 +24,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ],
     keywords="reinforcement-learning robotics gym jiminy",
     packages=find_namespace_packages(),
