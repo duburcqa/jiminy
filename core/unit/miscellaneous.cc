ddfbe27b20d1a12da348899dc5b32508db865946
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2024-02-04 08:49:11 +0100

    Jiminy release 1.7.19.post1 (#704)
    * [core] Fix broken flexible model.
    * [gym/toolbox] Improve numerical stability of 'quat_to_rpy' utility.
    * [misc] Prepare support of Linux ARM64.

--- core/unit/miscellaneous.cc
+++ core/unit/miscellaneous.cc
@@ -27,31 +27,45 @@ TEST(Miscellaneous, swapMatrixRows)
                      matrixOut);
     }
     {
-        Eigen::MatrixXi matrixIn(2, 9);
-        Eigen::MatrixXi matrixOut(2, 9);
-        // clang-format off
-        matrixIn << 0, 1, 2, 3, 4, 5, 6, 7, 8,
-                    0, 1, 2, 3, 4, 5, 6, 7, 8;
-        matrixOut << 0, 1, 2, 8, 6, 7, 3, 4, 5,
-                     0, 1, 2, 8, 6, 7, 3, 4, 5;
-        // clang-format on
-        swapMatrixRows(matrixIn.transpose(), 3, 3, 8, 1);
-        ASSERT_PRED2([](const Eigen::MatrixXi & lhs, const Eigen::MatrixXi & rhs)
+        Eigen::VectorXi matrixIn(9);
+        Eigen::VectorXi matrixOut(9);
+        matrixIn << 0, 1, 2, 3, 4, 5, 6, 7, 8;
+        matrixOut << 0, 6, 7, 8, 2, 3, 4, 5, 1;
+        swapMatrixRows(matrixIn, 1, 1, 6, 3);
+        ASSERT_PRED2([](const Eigen::VectorXi & lhs, const Eigen::VectorXi & rhs)
                      { return lhs.isApprox(rhs); },
                      matrixIn,
                      matrixOut);
     }
     {
-        Eigen::MatrixXi matrixIn(2, 9);
-        Eigen::MatrixXi matrixOut(2, 9);
-        // clang-format off
-        matrixIn << 0, 1, 2, 3, 4, 5, 6, 7, 8,
-                    0, 1, 2, 3, 4, 5, 6, 7, 8;
-        matrixOut << 0, 4, 5, 6, 7, 3, 1, 2, 8,
-                     0, 4, 5, 6, 7, 3, 1, 2, 8;
-        // clang-format on
-        swapMatrixRows(matrixIn.transpose(), 1, 2, 4, 4);
-        ASSERT_PRED2([](const Eigen::MatrixXi & lhs, const Eigen::MatrixXi & rhs)
+        Eigen::VectorXi matrixIn(9);
+        Eigen::VectorXi matrixOut(9);
+        matrixIn << 0, 1, 2, 3, 4, 5, 6, 7, 8;
+        matrixOut << 6, 7, 8, 1, 2, 3, 4, 5, 0;
+        swapMatrixRows(matrixIn, 0, 1, 6, 3);
+        ASSERT_PRED2([](const Eigen::VectorXi & lhs, const Eigen::VectorXi & rhs)
+                     { return lhs.isApprox(rhs); },
+                     matrixIn,
+                     matrixOut);
+    }
+    {
+        Eigen::VectorXi matrixIn(9);
+        Eigen::VectorXi matrixOut(9);
+        matrixIn << 0, 1, 2, 3, 4, 5, 6, 7, 8;
+        matrixOut << 0, 1, 2, 8, 6, 7, 3, 4, 5;
+        swapMatrixRows(matrixIn, 3, 3, 8, 1);
+        ASSERT_PRED2([](const Eigen::VectorXi & lhs, const Eigen::VectorXi & rhs)
+                     { return lhs.isApprox(rhs); },
+                     matrixIn,
+                     matrixOut);
+    }
+    {
+        Eigen::VectorXi matrixIn(9);
+        Eigen::VectorXi matrixOut(9);
+        matrixIn << 0, 1, 2, 3, 4, 5, 6, 7, 8;
+        matrixOut << 0, 4, 5, 6, 7, 3, 1, 2, 8;
+        swapMatrixRows(matrixIn, 1, 2, 4, 4);
+        ASSERT_PRED2([](const Eigen::VectorXi & lhs, const Eigen::VectorXi & rhs)
                      { return lhs.isApprox(rhs); },
                      matrixIn,
                      matrixOut);
--- python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/__init__.py
+++ python/gym_jiminy/toolbox/gym_jiminy/toolbox/math/__init__.py
@@ -2,18 +2,33 @@
 
 from .qhull import ConvexHull, compute_distance_convex_to_point
 from .generic import (squared_norm_2,
+                      matrix_to_quat,
+                      matrix_to_rpy,
                       matrix_to_yaw,
+                      quat_to_matrix,
+                      quat_to_rpy,
+                      quat_to_yaw,
                       quat_to_yaw_cos_sin,
-                      quat_to_yaw)
-
+                      quat_multiply,
+                      quat_average,
+                      rpy_to_matrix,
+                      rpy_to_quat)
 
 __all__ = [
     "ConvexHull",
     "compute_distance_convex_to_point",
     "squared_norm_2",
+    "matrix_to_quat",
+    "matrix_to_rpy",
     "matrix_to_yaw",
-    "quat_to_yaw_cos_sin",
+    "quat_to_matrix",
+    "quat_to_rpy",
     "quat_to_yaw",
+    "quat_to_yaw_cos_sin",
+    "quat_multiply",
+    "quat_average",
+    "rpy_to_matrix",
+    "rpy_to_quat"
 ]
 
 try:
