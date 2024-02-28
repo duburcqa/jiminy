af8f148f431a0715358696bd8ad912d4ff02f93d
Alexis Duburcq <alexis.duburcq@gmail.com>
2024-02-28 23:24:35 +0100

    [gym/envs] Fix broken 'Ant' environment.

--- python/gym_jiminy/envs/gym_jiminy/envs/ant.py
+++ python/gym_jiminy/envs/gym_jiminy/envs/ant.py
@@ -17,7 +17,7 @@ from pinocchio import (Quaternion,
 from jiminy_py.simulator import Simulator
 from gym_jiminy.common.bases import InfoType, EngineObsType
 from gym_jiminy.common.envs import BaseJiminyEnv
-from gym_jiminy.common.utils import sample
+from gym_jiminy.common.utils import sample, copyto
 
 if sys.version_info < (3, 9):
     from importlib_resources import files
@@ -52,7 +52,7 @@ class AntEnv(BaseJiminyEnv[np.ndarray, np.ndarray]):
             has_freeflyer=True, use_theoretical_model=False,
             config_path=config_path, debug=debug, **kwargs)
 
-        # Get the list of independant bodies (not connected via fixed joint)
+        # Get the list of independent bodies (not connected via fixed joint)
         self.body_indices = [0]  # World is part of bodies list
         for i, frame in enumerate(simulator.robot.pinocchio_model.frames):
             if frame.type == FrameType.BODY:
@@ -61,12 +61,17 @@ class AntEnv(BaseJiminyEnv[np.ndarray, np.ndarray]):
                 if frame_prev.type != FrameType.FIXED_JOINT:
                     self.body_indices.append(i)
 
-        # Observation chunks proxy for fast access
-        self.obs_chunks: List[np.ndarray] = []
-        self.obs_chunks_sizes: List[Tuple[int, int]] = []
-
         # Previous torso position along x-axis in world frame
-        self.xpos_prev = 0.0
+        self._xpos_prev = 0.0
+
+        # Define observation slices proxy for fast access.
+        # Note that they will be initialized in `_initialize_buffers`.
+        self._obs_slices: Tuple[np.ndarray] = ()
+
+        # Define base orientation and external forces proxies for fast access.
+        # Note that they will be initialized in `_initialize_buffers`.
+        self._base_rot = np.array([])
+        self._f_external:Tuple[np.ndarray] = ()
 
         # Initialize base class
         super().__init__(
@@ -120,9 +125,9 @@ class AntEnv(BaseJiminyEnv[np.ndarray, np.ndarray]):
         The observation space comprises:
 
             - robot configuration vector (absolute position (x, y) excluded),
-            - robot velocity vector,
-            - flatten external forces applied on each bodies in world frame
-              at robot center of mass.
+            - robot velocity vector (with base linear velocity in world frame),
+            - flatten external forces applied on each body in local frame, ie
+              centered at their respective center of mass.
         """
         # http://www.mujoco.org/book/APIreference.html#mjData
 
@@ -140,44 +145,50 @@ class AntEnv(BaseJiminyEnv[np.ndarray, np.ndarray]):
             np.full_like(velocity_space.high, np.inf),
             np.full(len(self.body_indices) * 6, 1.0)
         ])
-
         self.observation_space = gym.spaces.Box(
             low=low, high=high, dtype=np.float64)
 
-    def refresh_observation(self, measurement: EngineObsType) -> None:
-        # TODO: Do not rely on anything else than `measurement` to compute the
-        # observation, as anything else is not reliable.
+    def _initialize_buffers(self) -> None:
+        # Extract observation from the robot state.
+        # Note that this is only reliable with using a fixed step integrator.
+        engine_options = self.simulator.engine.get_options()
+        if engine_options['stepper']['odeSolver'] in ('runge_kutta_dopri5',):
+            raise ValueError(
+                "This environment does not support adaptive step integrators. "
+                "Please use either 'euler_explicit' or 'runge_kutta_4'.")
 
-        if not self.is_simulation_running:
-            # Initialize observation chunks
-            self.obs_chunks = [
+        # Initialize the base orientation as a rotation matrix
+        self._base_rot = self.robot.pinocchio_data.oMf[1].rotation
+
+        # Initialize vector of external forces
+        self._f_external = tuple(f.vector for f in self.robot_state.f_external)
+
+        # Re-initialize observation slices.
+        # Note that the base linear velocity is isolated as it will be computed
+        # on the fly and therefore updated separately.
+        obs_slices = []
+        obs_index_first = 0
+        for data in (
                 self._robot_state_q[2:],
-                self._robot_state_v,
-                *[f.vector for f in self.robot_state.f_external]
-            ]
+                self._robot_state_v[:3],
+                self._robot_state_v[3:],                *self._f_external):
+           obs_index_last = obs_index_first + len(data)
+           obs_slices.append(self.observation[obs_index_first:obs_index_last])
+           obs_index_first = obs_index_last
+        self._obs_slices = (obs_slices[0], *obs_slices[2:], obs_slices[1])
 
-            # Initialize observation chunks sizes
-            self.obs_chunks_sizes = []
-            idx_start = 0
-            for obs in self.obs_chunks:
-                idx_end = idx_start + len(obs)
-                self.obs_chunks_sizes.append((idx_start, idx_end))
-                idx_start = idx_end
+        # Initialize previous torso position along x-axis
+        self._xpos_prev = self._robot_state_q[0]
 
-            # Initialize previous torso position
-            self.xpos_prev = self._robot_state_q[0]
-
-        # Update observation buffer
-        assert isinstance(self.observation_space, gym.spaces.Box)
-        for obs, size in zip(self.obs_chunks, self.obs_chunks_sizes):
-            obs_slice = slice(*size)
-            low = self.observation_space.low[obs_slice]
-            high = self.observation_space.high[obs_slice]
-            obs.clip(low, high, out=self.observation[obs_slice])
+    def refresh_observation(self, measurement: EngineObsType) -> None:
+        # Update observation
+        copyto(self._obs_slices[:-1], (
+            self._robot_state_q[2:],
+            self._robot_state_v[3:],
+            *self._f_external))
 
         # Transform observed linear velocity to be in world frame
-        self.observation[slice(*self.obs_chunks_sizes[1])][:3] = \
-            Quaternion(self._robot_state_q[3:7]) * self.obs_chunks[1][:3]
+        self._obs_slices[-1][:] = self._base_rot @ self._robot_state_v[:3]
 
     def has_terminated(self) -> Tuple[bool, bool]:
         """ TODO: Write documentation.
@@ -203,12 +214,12 @@ class AntEnv(BaseJiminyEnv[np.ndarray, np.ndarray]):
 
         # Compute forward velocity reward
         xpos = self._robot_state_q[0]
-        forward_reward = (xpos - self.xpos_prev) / self.step_dt
+        forward_reward = (xpos - self._xpos_prev) / self.step_dt
 
         ctrl_cost = 0.5 * np.square(self.action).sum()
 
-        obs_slice = slice(self.obs_chunks_sizes[2][0],
-                          self.obs_chunks_sizes[-1][1])
+        obs_slice = slice(self.obs_chunk_slices[2].start(),
+                          self.obs_chunk_slices[-1].stop())
         f_ext = self.observation[obs_slice]
         contact_cost = 0.5 * 1e-3 * np.square(f_ext).sum()
 
@@ -224,6 +235,6 @@ class AntEnv(BaseJiminyEnv[np.ndarray, np.ndarray]):
         })
 
         # Update previous torso forward position buffer
-        self.xpos_prev = xpos
+        self._xpos_prev = xpos
 
         return reward
