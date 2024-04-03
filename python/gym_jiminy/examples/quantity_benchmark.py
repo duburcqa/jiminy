08151e36ab44a58d38ae20c4d0b19e470fc86e75
Alexis Duburcq <alexis.duburcq@gmail.com>
2024-04-03 14:01:58 +0200

    [misc] Add quantity benchmark example script.

---
+++ python/gym_jiminy/examples/quantity_benchmark.py
@@ -0,0 +1,59 @@
+import timeit
+
+import matplotlib.pyplot as plt
+import gymnasium as gym
+
+import gym_jiminy.common.bases.quantity
+from gym_jiminy.common.bases import QuantityManager
+from gym_jiminy.common.quantities import EulerAnglesFrame
+
+# Define number of samples for benchmarking
+N_SAMPLES = 20000
+
+# Disable caching by forcing `SharedCache.has_value` to always return `False`
+setattr(gym_jiminy.common.bases.quantity.SharedCache,
+        "has_value",
+        property(lambda self: False))
+
+# Instantiate a dummy environment
+env = gym.make("gym_jiminy.envs:atlas")
+env.reset()
+env.step(env.action)
+
+# Define quantity manager and add quantities to benchmark
+nframes = len(env.pinocchio_model.frames)
+quantity_manager = QuantityManager(
+    env.simulator,
+    {
+        f"rpy_{i}": (EulerAnglesFrame, dict(frame_name=frame.name))
+        for i, frame in enumerate(env.pinocchio_model.frames)
+    })
+
+# Run the benchmark for all batch size
+time_per_frame_all = []
+for i in range(1, nframes):
+    # Reset tracking
+    quantity_manager.reset(reset_tracking=True)
+
+    # Fetch all quantities once to update dynamic computation graph
+    for j, quantity in enumerate(quantity_manager.quantities.values()):
+        quantity.get()
+        if i == j + 1:
+            break
+
+    # Extract batched data buffer of `EulerAnglesFrame` quantities
+    shared_data = quantity.requirements['data']
+
+    # Benchmark computation of batched data buffer
+    duration = timeit.timeit(
+        'shared_data.get()', number=N_SAMPLES, globals={
+            "shared_data": shared_data
+        })
+    time_per_frame_all.append(duration / N_SAMPLES / i * 1e9)
+
+# Plot the result
+plt.figure()
+plt.plot(time_per_frame_all)
+plt.xlabel("Number of frames")
+plt.ylabel("Average computation time per frame (ns)")
+plt.show()
