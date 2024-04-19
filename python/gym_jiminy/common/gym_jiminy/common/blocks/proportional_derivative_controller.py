895e7fe9ceaf1ad37c8ad5d7386ee5488690e9a9
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2024-04-19 08:04:20 +0200

    [gym/common] Fix PD controller for unbounded acceleration. (#768)

--- python/gym_jiminy/common/gym_jiminy/common/blocks/proportional_derivative_controller.py
+++ python/gym_jiminy/common/gym_jiminy/common/blocks/proportional_derivative_controller.py
@@ -71,9 +71,11 @@ def integrate_zoh(state: np.ndarray,
     # when hitting position bounds.
     horizon = np.maximum(
         np.floor(np.abs(velocity_prev) / acceleration_max / dt) * dt, dt)
-    drift = (horizon * (horizon - dt)) / 2.0
-    position_min_delta = position_min - (position + drift * acceleration_max)
-    position_max_delta = position_max - (position - drift * acceleration_max)
+    position_min_delta = position_min - position
+    position_max_delta = position_max - position
+    drift = 0.5 * (horizon[horizon > dt] * (horizon[horizon > dt] - dt))
+    position_min_delta[horizon > dt] -= drift * acceleration_max[horizon > dt]
+    position_max_delta[horizon > dt] += drift * acceleration_max[horizon > dt]
     velocity_min = position_min_delta / horizon
     velocity_max = position_max_delta / horizon
     velocity[:] = np.minimum(np.maximum(velocity, velocity_min), velocity_max)
