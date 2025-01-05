[gym_jiminy/common] Fix 'evaluate' total reward computation.

python/gym_jiminy/common/gym_jiminy/common/envs/generic.py
@@ -234,9 +234,6 @@ class BaseJiminyEnv(InterfaceJiminyEnv[Obs, Act],
         # Information about the learning process
         self._info: InfoType = {}
 
-        # Keep track of cumulative reward
-        self.total_reward = 0.0
-
         # Number of simulation steps performed
         self.num_steps = np.array(-1, dtype=np.int64)
         self._num_steps_beyond_terminate: Optional[int] = None
@@ -719,9 +716,6 @@ class BaseJiminyEnv(InterfaceJiminyEnv[Obs, Act],
                 "The simulation has already terminated at `reset`. Check the "
                 "implementation of `has_terminated` if overloaded.")
 
-        # Reset cumulative reward
-        self.total_reward = 0.0
-
         # Note that the viewer must be reset if available, otherwise it would
         # keep using the old robot model for display, which must be avoided.
         if self.simulator.is_viewer_available:
@@ -858,9 +852,6 @@ class BaseJiminyEnv(InterfaceJiminyEnv[Obs, Act],
                 raise RuntimeError(
                     "The reward is 'nan'. Something went wrong with "
                     "`compute_reward` implementation.")
-
-            # Update cumulative reward
-            self.total_reward += reward
 
         # Clip (and copy) the most derived observation before returning it
         obs = self._get_clipped_env_observation()
@@ -1177,14 +1168,12 @@ class BaseJiminyEnv(InterfaceJiminyEnv[Obs, Act],
         if is_training:
             self.eval()
 
-        # Set the seed without forcing full reset of the environment
-        self._initialize_seed(seed)
-
         # Initialize the simulation
-        obs, info = env.reset()
+        obs, info = env.reset(seed=seed)
         action, reward, terminated, truncated = None, None, False, False
 
         # Run the simulation
+        total_reward = 0.0
         info_episode = [info]
         try:
             while horizon is None or self.num_steps < horizon:
@@ -1197,6 +1186,7 @@ class BaseJiminyEnv(InterfaceJiminyEnv[Obs, Act],
                     break
                 obs, reward, terminated, truncated, info = env.step(action)
                 info_episode.append(info)
+                total_reward += float(reward)
             env.stop()
         except KeyboardInterrupt:
             pass
@@ -1208,7 +1198,7 @@ class BaseJiminyEnv(InterfaceJiminyEnv[Obs, Act],
         # Display some statistic if requested
         if enable_stats:
             print("env.num_steps:", self.num_steps)
-            print("cumulative reward:", self.total_reward)
+            print("cumulative reward:", total_reward)
 
         # Replay the result if requested
         if enable_replay: